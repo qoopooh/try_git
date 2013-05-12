5074df55591dd052778ec87e6e3e9419563ca05f
Berm Arch64 Mac <berm@arch64.mac>
2013-05-12 11:27:50 +0700

    hello world with new icon

---
+++ node/modularity.js
@@ -0,0 +1,13 @@
+(function(){
+  var local = "this is local, thanks to var";
+  
+  nonLocal = function() {
+    console.log(local);
+    return local;
+  }
+})();
+
+console.log(typeof nonLocal == "function"); // true
+console.log(typeof nonLocal() == "string"); // true
+/*console.log(local); // not defined*/
+
