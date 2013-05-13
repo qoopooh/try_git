c581957dc718bc021afe39aba2b10a8af28bb9b4
Berm Mini <mac@mini.berm>
2013-05-13 22:47:34 +0700

    call apply

--- node/person.js
+++ node/person.js
@@ -18,15 +18,37 @@ console.log(personFullNameReverse(s));
 function Person(first, last) {
   this.first = first;
   this.last = last;
-  this.fullName = function() {
-    return this.first + ' ' + this.last;
+}
+Person.prototype.fullName = function() {
+  return this.first + ' ' + this.last;
+}
+Person.prototype.fullNameReversed = function() {
+  return this.last + ', ' + this.first;
+}
+Person.prototype.firstNameCaps = function() {
+  return this.first.toUpperCase();
+}
+Person.prototype.reversed = function() {
+  var r = "";
+  var full = this.fullName();
+  for (var i = full.length - 1; i >= 0; i--) {
+    r += full[i];
   }
-  this.fullNameReversed = function() {
-    return this.last + ', ' + this.first;
-  }
+
+  return r;
+}
+Person.prototype.toString = function() {
+  return '<Person: ' + this.fullName() + '>';
+}
+function lastNameCaps() {
+  return this.last.toUpperCase();
 }
 
 var p = new Person("Phuchit", "Kanjanakhan");
-console.log(p.fullNameReversed());
-
+console.log(p.firstNameCaps());
+console.log(p.reversed());
+console.log(p.toString());
+console.log(lastNameCaps);
+console.log(lastNameCaps.call(p));
+console.log(personFullNameReverse.apply(null, [p]));
 
