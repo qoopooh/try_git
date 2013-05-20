4bbadee97e257a287fc76a41264c10fb4ba44e1e
Berm Arch32 AAE015 <berm@arch32.aae015>
2013-05-20 17:17:22 +0700

    runtime error 'systemd-journal'

--- qt5/stx10-shelf-bag/trunk/mainwindow.cpp
+++ qt5/stx10-shelf-bag/trunk/mainwindow.cpp
@@ -7,7 +7,6 @@ MainWindow::MainWindow(QWidget *parent) :
     vcom(false)
 {
     ui->setupUi(this);
-    ui->progressBar->setValue (0);
 
     stReader = new StReader(this);
     bootloader = new BootLoader(this);
@@ -23,12 +22,7 @@ MainWindow::MainWindow(QWidget *parent) :
     clk10msTimer->start(10);
     clk10msCounter = 0;
 
-    flashState = FLASH_STATE_IDLE;
     ui->statusBar->showMessage(tr("Started!"));
-
-    //debug
-    ui->lineEditFilepath->setText(tr("D:\\Installed Files\\qt\\firmwareUpdate\\ST-110_1.0.0_1.0.3V.aae"));
-    //debug
 }
 
 MainWindow::~MainWindow()
@@ -70,72 +64,11 @@ void MainWindow::on_checkBoxConnect_clicked(bool checked)
 void MainWindow::setConnectingControl(bool connect)
 {
     ui->groupBoxConnection->setEnabled (!connect);
-    ui->groupBoxFirmware->setEnabled (connect);
+    //ui->groupBoxFirmware->setEnabled (connect);
 }
 
-void MainWindow::on_pushButtonBrowse_clicked()
-{
-    QString fileName;
-    QString caption;
-//    QString directory = QApplication::applicationDirPath();
-    QString filter;
-
-    if (ui->radioButtonSt->isChecked ()) {
-        caption = tr("Open AAE File");
-        filter = tr("AAE Files (*.aae)");
-    } else if (ui->radioButtonPr->isChecked ()) {
-        caption = tr("Open MOT File");
-        filter = tr("MOT Files (*.mot)");
-    } else {
-        caption = tr("Open File");
-        filter = tr("Files (*.*)");
-    }
-
-    fileName = QFileDialog::getOpenFileName(this, caption, "", filter);
-    if (fileName != "") {
-        ui->lineEditFilepath->setText (fileName);
-    }
-}
-void MainWindow::on_pushButtonFlash_clicked()
-{
-    int answer;
-//    quint32 size;
-
-    m_File = ui->lineEditFilepath->text ();
-
-    if (m_File.contains (tr("V.aae"), Qt::CaseSensitive)) {
-        answer = QMessageBox::information( this, "Warning:: Wrong firmware",
-                      "This firmware is normally used for STx10 USB version.\n"
-                      "Do you still want to flash now?",
-                      "Yes", "No",
-                      0, 1 );
-        if (answer == 1) {
-            return;
-        }
-        vcom = true;
-    } else {
-        vcom = false;
-    }
-
-    hexFile.setFileName(m_File);
-    if (hexFile.open(QIODevice::ReadOnly)) {
-//        size = hexFile.size();
-        m_Memory.clear();
-
-        while (!hexFile.atEnd()) {
-            QByteArray line = hexFile.readLine();
-            m_Memory.append(line);
-        }
-
-        ui->listWidgetLog->clear();
-        flashState = FLASH_STATE_GOTOBL;
-        stReader->setUsbReader(vcom);
-        hexFile.close();
-    }
-}
 void MainWindow::onReaderPacketIn(const QByteArray &input)
 {
-    //qDebug() << "item: " << input;
     ui->listWidgetLog->addItem(QString(input));
     ui->listWidgetLog->scrollToBottom();
 }
@@ -145,81 +78,6 @@ void MainWindow::on10msTimer()
     clk10msCounter++;
 
     if ((clk10msCounter % 50) == 0) {
-        handleFlashState();
+        //handleFlashState();
     }
-}
-void MainWindow::handleFlashState()
-{
-    switch (flashState) {
-    case FLASH_STATE_IDLE:
-        break;
-    case FLASH_STATE_GOTOBL:
-        if (stReader->gotoBootloaderMode()) {
-            stReader->disconnectReader();
-            connect(bootloader, SIGNAL(connection(bool)), this, SLOT(setConnectingControl(bool)));
-            connect(bootloader, SIGNAL(raiseErrorMessage(QString)), ui->statusBar, SLOT(showMessage(QString)));
-            connect(bootloader, SIGNAL(raiseStatusMessage(QString)), ui->statusBar, SLOT(showMessage(QString)));
-            connect(bootloader, SIGNAL(nextpage(int)), this, SLOT(onFlashNextPage(int)));
-            bootloader->connectReader(channel);
-            bootloader->writeFirmware(m_Memory);
-            bootloader->writeFirmwareStep();
-
-            clk10msFlashNextpageTimeout = clk10msCounter + 500;
-            ui->groupBoxFirmware->setEnabled(false);
-            flashState = FLASH_STATE_FLASHING;
-        }
-        break;
-    case FLASH_STATE_FLASHING:
-        if (clk10msCounter > clk10msFlashNextpageTimeout) {
-            flashState = FLASH_STATE_FAIL;
-        }
-        break;
-    case FLASH_STATE_FLASHED:
-        bootloader->disconnectReader();
-        disconnect(bootloader, SIGNAL(connection(bool)), this, SLOT(setConnectingControl(bool)));
-        disconnect(bootloader, SIGNAL(raiseErrorMessage(QString)), ui->statusBar, SLOT(showMessage(QString)));
-        disconnect(bootloader, SIGNAL(raiseStatusMessage(QString)), ui->statusBar, SLOT(showMessage(QString)));
-        disconnect(bootloader, SIGNAL(nextpage(int)), this, SLOT(onFlashNextPage(int)));
-        stReader->connectReader();
-
-        ui->groupBoxFirmware->setEnabled(false);
-        flashState = FLASH_STATE_GOTOFW;
-        break;
-    case FLASH_STATE_GOTOFW:
-        if (stReader->gotoFirmwareMode()) {
-            flashState = FLASH_STATE_IDLE;
-            //qDebug() << "-------- finished ----------";
-        }
-        break;
-    case FLASH_STATE_FAIL:
-        QMessageBox::information(this, "Flashing failed", "Please restart reader", "OK");
-
-        bootloader->disconnectReader();
-        disconnect(bootloader, SIGNAL(connection(bool)), this, SLOT(setConnectingControl(bool)));
-        disconnect(bootloader, SIGNAL(raiseErrorMessage(QString)), ui->statusBar, SLOT(showMessage(QString)));
-        disconnect(bootloader, SIGNAL(nextpage(int)), this, SLOT(onFlashNextPage(int)));
-
-        flashState = FLASH_STATE_IDLE;
-        //qDebug() << "-------- FLASH_STATE_FAIL ----------";
-        break;
-    default:
-        flashState = FLASH_STATE_IDLE;
-        break;
-    }
-}
-
-void MainWindow::onFlashNextPage(int progressPercentage)
-{
-    ui->progressBar->setValue(progressPercentage);
-    clk10msFlashNextpageTimeout = clk10msCounter + 500;
-    if (bootloader->writeFirmwareStep()) {
-        flashState = FLASH_STATE_FLASHED;
-    }
-}
-
-void MainWindow::on_pushButtonTest_clicked()
-{
-//    stReader->sendTestData();
-//    stReader->gotoBootloaderMode();
-//    stReader->gotoFirmwareMode();
 }
--- qt5/stx10-shelf-bag/trunk/mainwindow.h
+++ qt5/stx10-shelf-bag/trunk/mainwindow.h
@@ -12,14 +12,6 @@ namespace Ui {
     class MainWindow;
 }
 
-// ----------------------------------------------------------------------------
-// Set this to the maximum value of 63kB (for 'F340) because this needs to be
-// known at compile-time. An array of this size is allocated as "m_Memory" in
-#define MEMORY_SIZE 63*1024
-#define APP_START_PAGE        0x09     // First application page (page 09)
-#define PAGE_SIZE             0x200    // Pages are 512 bytes long
-#define APP_START_ADDR        APP_START_PAGE*PAGE_SIZE
-
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
@@ -42,14 +34,9 @@ public:
 
 private slots:
     void on_pushButtonRefresh_clicked();
-    void on_pushButtonBrowse_clicked();
-    void on_pushButtonFlash_clicked();
     void setConnectingControl(bool connect);
     void onReaderPacketIn(const QByteArray &input);
     void on10msTimer();
-    void onFlashNextPage(int progressPercentage);
-
-    void on_pushButtonTest_clicked();
 
     void on_checkBoxConnect_clicked(bool checked);
 
@@ -62,19 +49,8 @@ private:
 
     QTimer *clk10msTimer;
     quint32 clk10msCounter;
-    quint32 clk10msFlashNextpageTimeout;
-    quint32 clk10msRebootDelay;
-    /* Boot Loader */
-    QString m_File;
-    QFile hexFile;
-    QByteArray m_Memory;
-    FLASH_STATE flashState;
-    /* Boot Loader */
 
     void getReaderChannels();
-    /* Boot Loader */
-    void handleFlashState();
-    /* Boot Loader */
 };
 
 #endif // MAINWINDOW_H
