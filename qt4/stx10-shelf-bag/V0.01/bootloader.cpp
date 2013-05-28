20280c9ee77ad2adf783063c584a42d2d5aad373
Berm Arch32 AAE015 <berm@arch32.aae015>
2013-05-28 09:12:34 +0700

    V0.01 Release

---
+++ qt4/stx10-shelf-bag/V0.01/bootloader.cpp
@@ -0,0 +1,70 @@
+#include "bootloader.h"
+#include "XonXoff.h"
+
+BootLoader::BootLoader(QObject *parent) :
+    AaeReader(parent)
+{
+    setBaudrate(BAUD115200);
+    dataReceivedQueue.clear();
+    blReceiveState = BL_RCV_UNDEF;
+}
+
+void BootLoader::onReadyRead()
+{
+    dataReceivedByteArray.clear();
+    dataReceivedByteArray = readDataFromConnection();
+    responseFlag = true;
+
+    foreach (char ch, dataReceivedByteArray) {
+        dataReceivedQueue.enqueue (ch);
+    }
+    while (dataReceivedQueue.length () > 0) {
+        switch (blReceiveState) {
+        case BL_RCV_XON:
+            if (dataReceivedQueue.dequeue() == XON) {
+                blReceiveState = BL_RCV_XOFF;
+            }
+            break;
+        case BL_RCV_XOFF:
+            if (dataReceivedQueue.dequeue() == XOFF) {
+                blReceiveState = BL_RCV_XON;
+                updateFlashProgress();
+            }
+            break;
+        default:
+            if (dataReceivedQueue.dequeue() == XON) {
+                blReceiveState = BL_RCV_XOFF;
+            } else {
+                blReceiveState = BL_RCV_XON;
+                updateFlashProgress();
+            }
+            break;
+        }
+    }
+}
+void BootLoader::updateFlashProgress()
+{
+    flashingProgressPercentage = (pageCounter * 100) / pageNumber;
+    emit nextpage(flashingProgressPercentage);
+    emit raiseStatusMessage(tr("Flashing progress %1%").arg(flashingProgressPercentage));
+}
+
+quint16 BootLoader::writeFirmware(const QByteArray &_flashMemory)
+{
+    pageCounter = 0;
+    flashMemory = &_flashMemory;
+    pageNumber = flashMemory->size() / FIRMWARE_PAGE_SIZE;
+
+    return pageNumber;
+}
+bool BootLoader::writeFirmwareStep()
+{
+    sendDataToReader(flashMemory->mid(pageCounter * FIRMWARE_PAGE_SIZE, FIRMWARE_PAGE_SIZE));
+
+    pageCounter++;
+    if (pageCounter > pageNumber) {
+        return true;
+    }
+    return false;
+}
+
---
+++ qt4/stx10-shelf-bag/V0.01/mainwindow.h
@@ -0,0 +1,78 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QFileDialog>
+#include <QMessageBox>
+#include <QTimer>
+#include <QResizeEvent>
+#include <QListWidgetItem>
+#include "streader.h"
+#include "aaecommand.h"
+#include "bootloader.h"
+#include "epctreemodel.h"
+
+namespace Ui {
+    class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    enum FLASH_STATE{
+        FLASH_STATE_IDLE,
+        FLASH_STATE_GOTOBL,
+        FLASH_STATE_GOTOBL_DELAY,
+        FLASH_STATE_STARTBL,
+        FLASH_STATE_FLASHING,
+        FLASH_STATE_FLASHED,
+        FLASH_STATE_GOTOFW,
+        FLASH_STATE_FAIL,
+        FLASH_STATE_UNDEF = 0xFE
+    };
+
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+protected:
+    void resizeEvent(QResizeEvent *event);
+    void createLogTable();
+
+private slots:
+    void on_pushButtonRefresh_clicked();
+    void setConnectingControl(bool connect);
+    void onReaderPacketIn(const QByteArray &input);
+    void onEpc(const QByteArray &);
+    void onEpcString(const QString &);
+    void on10msTimer();
+
+    void on_checkBoxConnect_clicked(bool checked);
+    void on_pushButtonStart_clicked();
+    void on_pushButtonStop_clicked();
+    void on_pushButtonSingle_clicked();
+    void on_pushButtonClear_clicked();
+
+private:
+    Ui::MainWindow *ui;
+    EpcTreeModel *model;
+    StReader *stReader;
+    BootLoader *bootloader;
+    QString channel;
+    bool vcom;
+    QString prev_epc;
+    int prev_epc_count;
+    int shelf_changed_tout;
+    int bag_changed_tout;
+
+    QTimer *clk10msTimer;
+    quint32 clk10msCounter;
+
+    void getReaderChannels();
+    void insertDupplicatedTag(const QString);
+    void setShelfAndBag(const QString);
+    void updateActions();
+};
+
+#endif // MAINWINDOW_H
