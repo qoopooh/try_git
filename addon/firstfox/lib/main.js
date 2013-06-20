45799636a7116fc07eb6f9660bb30eb5641de93c
Berm Arch32 AAE015 <berm@arch32.aae015>
2013-06-20 13:52:38 +0700

    right/left click

--- addon/firstfox/lib/main.js
+++ addon/firstfox/lib/main.js
@@ -1,12 +1,23 @@
 var widgets = require("sdk/widget");
 var tabs = require("sdk/tabs");
+var self = require("sdk/self");
  
 var widget = widgets.Widget({
   id: "mozilla-link",
   label: "Mozilla website",
-  contentURL: "http://www.mozilla.org/favicon.ico",
-  onClick: function() {
-    tabs.open("http://www.mozilla.org/");
-  }
+/*  contentURL: "http://www.mozilla.org/favicon.ico",*/
+  contentURL: self.data.url("ic_identify.png"),
+/*  onClick: function() {*/
+/*    tabs.open("http://google.com/");*/
+/*  },*/
+  contentScriptFile: self.data.url("click-listener.js")
+});
+ 
+widget.port.on("left-click", function(){
+  console.log("left-click");
+});
+ 
+widget.port.on("right-click", function(){
+  console.log("right-click");
 });
 
