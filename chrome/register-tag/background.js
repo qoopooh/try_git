4668f904284fe88bd4435024eaf96816b996f585
Berm Arch32 AAE015 <berm@arch32.aae015>
2013-07-24 13:56:01 +0700

    screen side

--- chrome/register-tag/background.js
+++ chrome/register-tag/background.js
@@ -2,13 +2,13 @@ var appWindow;
 
 chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create('main.html', {
-    minWidth: 330,
-    minHeight: 355,
+    minWidth: 300,
+    minHeight: 280,
     bounds: {
       top: 20,
       left: 20,
-      width: 720,
-      height: 473
+      width: 700,
+      height: 400
     }
   }, function(app) {
     app.contentWindow.onclose = function() {
