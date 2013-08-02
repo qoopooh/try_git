eb68471e94a315057746ca004aee66c2be662c76
lmde64 <pooh@lmde>
2013-08-02 16:57:55 +0700

    have to set watchdog

--- msp430/blink/main.c
+++ msp430/blink/main.c
@@ -1,12 +1,25 @@
 #include <legacymsp430.h>
 #include "def.h"
 
-void InitializeLeds(void);
+void InitializeLeds(void)
+{
+  LED_DIR |= LED1 + LED2;                          
+  LED_OUT &= ~(LED1 + LED2);  
+}
+
+/** Delay function. **/
+void delay(unsigned int d)
+{
+  int i;
+
+  for (i = 0; i<d; i++) {
+    nop();
+  }
+}
 
 int main(void)
 {
-  unsigned char i, j;
-
+  WDTCTL = WDTPW | WDTHOLD; // Must have
   BCSCTL3 |= LFXT1S_2;
   TACTL |= TASSEL_1 | MC_1;
   TACCTL0 = CCIE;
@@ -15,24 +28,15 @@ int main(void)
   InitializeLeds();
 
   while(1) {
-    for (i=0; i<100; ++i) {
-      for (j=0; j<10; ++j) {
-      }
-    }
-    LED_OUT ^= (LED1);
+    LED_OUT ^= (LED2);
+    delay(0x4fff);
   }
 
   return 0;
 }
 
-void InitializeLeds(void)
-{
-  LED_DIR |= LED1 + LED2;                          
-  LED_OUT &= ~(LED1 + LED2);  
-}
-
 interrupt(TIMERA0_VECTOR) TIMERA0_ISR(void)
 {
-  LED_OUT ^= (LED2);
+  LED_OUT ^= (LED1);
 }
 
