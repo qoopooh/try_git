15b0e2f671d3e22a59fe8bd63ab55d504e561690
Berm Mini <mac@mini.berm>
2013-08-18 21:44:28 +0700

    modify window

--- chrome/aia20plnp/background.js
+++ chrome/aia20plnp/background.js
@@ -1,7 +1,7 @@
 chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create('window.html', {
-    id: 'aia', // to remember position
-    frame: 'chrome', bounds: { width: 320, height: 600 },
+    /*id: 'aia', // to remember position*/
+    frame: 'chrome', bounds: { width: 320, height: 300 },
     minWidth:200, minHeight:140, maxHeight:600
   });
 });
