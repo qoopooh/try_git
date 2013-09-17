ac7dce639c10d595ab43cd841f4be9612283ebe2
pooh arch64 <pooh@arch64.n570>
2013-09-17 22:17:53 +0700

    back quote

---
+++ shell/arith.sh
@@ -0,0 +1,8 @@
+expr 1 + 3        # 4
+expr 2 - 1        # 1
+expr 10 / 2       # 5
+expr 20 % 3       # 2
+expr 10 \* 3      # 30
+echo `expr 6 + 3` # 9
+echo "expr 6 + 3" # expr 6 + 3
+
--- shell/ginfo.sh
+++ shell/ginfo.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
 clear
-echo "Hello $USER"
+echo "Hello $USER, you're in `pwd`"
 echo "Today is $(date)"
-echo "Number of user login: " ; who | wc -l # Do not work on wrt
+echo -n "Number of user login: " ; who | wc -l # output on same line | do not work on wrt
+echo -e "This os is \c"   # \c for suppressing new line
+uname -a
 echo "Calendar"
 cal # Do not work on wrt
+
 
 myname=Vivek
 myos=TroubleOS
