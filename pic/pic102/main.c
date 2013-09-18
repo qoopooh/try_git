acfdf23766dee88cf273f446c6a227d96bcaf03a
Berm Arch32 AAE015 <berm@arch32.aae015>
2013-09-18 18:00:26 +0700

    follow http://www.engscope.com/pic24-tutorial/5-inputs-and-outputs/

--- pic/pic102/main.c
+++ pic/pic102/main.c
@@ -1,5 +1,4 @@
-#include <stdio.h>
-#include "p24Fxxxx.h"
+#include "p24FJ256GB106.h"
 
 void InitIO(void);
 
@@ -8,20 +7,24 @@ _CONFIG2( FNOSC_PRI & POSCMOD_XT )
 
 void InitIO(void)
 {
-  //LATA = 0x0001;
-  //LATB = 0x8004;
+  LATB = 0x8004;
 
   AD1PCFG = 0x1FFF;
 
-  //TRISA = 0x0000; 
-  TRISB = 0x77C8;
+  AD1PCFGL  =  0xFFFF;  //set to all digital I/O
+  TRISB  =  0xFFFF;  //configure all PortB as input
+  TRISC   =  0xFFFF;  //configure all PortC as input,
+
+  RPINR18bits.U1RXR   =  2;   //UART1 receive set to RB2/RP2
+  RPOR1bits.RP3R   =  3;   //UART1 transmit set to RB3/RP3
 }
 
 int main (void)
 {
-  PMD1 = 0x00F9;
-  PMD2 = 0x1F1E;
-  PMD3 = 0x0780;
+  OSCCON = 0x11C0;  //select INTERNAL RC, Post Scale PPL
+  //PMD1 = 0x00F9;
+  //PMD2 = 0x1F1E;
+  //PMD3 = 0x0780;
   InitIO();
 
   while (1) {
