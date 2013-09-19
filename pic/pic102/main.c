0f1c868c445e9262e23e2d4ebc1af8c2f28be94e
Berm Arch32 AAE015 <berm@arch32.aae015>
2013-09-19 08:47:36 +0700

    can run something

--- pic/pic102/main.c
+++ pic/pic102/main.c
@@ -1,30 +1,24 @@
 #include "p24FJ256GB106.h"
 
-void InitIO(void);
-
-_CONFIG1( JTAGEN_OFF & FWDTEN_OFF)
-_CONFIG2( FNOSC_PRI & POSCMOD_XT )
-
 void InitIO(void)
 {
   LATB = 0x8004;
 
-  AD1PCFG = 0x1FFF;
+  //AD1PCFG = 0xFFFF; //set to all digital I/O
 
-  AD1PCFGL  =  0xFFFF;  //set to all digital I/O
-  TRISB  =  0xFFFF;  //configure all PortB as input
+  TRISB  =  0xFBFF;  //configure all PortB as input
   TRISC   =  0xFFFF;  //configure all PortC as input,
+  TRISD   =  0xFF00;
+  TRISF   =  0xFFFC;  // RF0-1 are relays
 
-  RPINR18bits.U1RXR   =  2;   //UART1 receive set to RB2/RP2
-  RPOR1bits.RP3R   =  3;   //UART1 transmit set to RB3/RP3
+  LATBbits.LATB10 = 1;
+  LATDbits.LATD5 = 1;
+  LATF = 0x0003;
 }
 
 int main (void)
 {
-  OSCCON = 0x11C0;  //select INTERNAL RC, Post Scale PPL
-  //PMD1 = 0x00F9;
-  //PMD2 = 0x1F1E;
-  //PMD3 = 0x0780;
+  OSCCON = 0x2200;
   InitIO();
 
   while (1) {
