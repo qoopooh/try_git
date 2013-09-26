e398df61a0aa831fcbee632820b1fb2320732fd2
Berm Mini <mac@mini.berm>
2013-09-26 21:40:33 +0700

    get กองทุน

--- python/http.py
+++ python/http.py
@@ -1,20 +1,28 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 import urllib2
+import types
+
 from HTMLParser import HTMLParser
 
 class MyHTMLParser(HTMLParser):
   def handle_starttag(self, tag, attrs):
-    print 'start ', tag
+#print 'start ', tag
+    return
   def handle_endtag(self, tag):
-    print 'end ', tag
+#print 'end ', tag
+    return
   def handle_data(self, data):
-    if type(data) == 'unicode':
-      print 'dat ', data.decode('utf-8')
+    u = unicode(data, 'tis-620')
+    if u'กอง' in u:
+      print 'dat ', u
 
-req = urllib2.Request(url='https://direct.aia.co.th/btob/FundQueryServlet?actionType=show',
-    data='This data is passed to stdin of the CGI')
-f = urllib2.urlopen(req)
+#req = urllib2.Request(url='https://direct.aia.co.th/btob/FundQueryServlet?actionType=show',
+#data='This data is passed to stdin of the CGI')
+#f = urllib2.urlopen(req)
+
+f = open('fund.html', 'r')
 
 #print f.read()
 parser = MyHTMLParser()
