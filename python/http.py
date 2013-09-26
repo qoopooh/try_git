eccd7fdd96a4760d123a642f77b707175b8f54d1
Berm Mini <mac@mini.berm>
2013-09-27 00:02:16 +0700

    prepare json

--- python/http.py
+++ python/http.py
@@ -2,49 +2,63 @@
 # -*- coding: utf-8 -*-
 
 import urllib2
-import types
+import json
 
 from HTMLParser import HTMLParser
 
 
 class MyHTMLParser(HTMLParser):
-  sell, buy = 0, 0
-  prices, fund = [], []
+  __sell, __buy = 0, 0
+  prices, __fund = [], []
 
   def handle_starttag(self, tag, attrs):
-#print 'start ', tag
+    #print 'start ', tag
     return
   def handle_endtag(self, tag):
-#print 'end ', tag
     return
   def handle_data(self, data):
     u = unicode(data, 'tis-620')
     if u'กองทุนเปิด' in u:
-      self.fund = []
-      self.fund.append(u)
-      self.sell = 5
-    elif self.sell > 0:
-      self.sell -= 1
-      if self.sell == 0:
-        self.fund.append(data)
-        self.buy = 2
-    elif self.buy > 0:
-      self.buy -= 1
-      if self.buy == 0:
-        self.fund.append(data)
-        self.prices.append(self.fund)
-  def pricelist(self):
-    for f in self.prices:
+      self.__fund = []
+      self.__fund.append(u)
+      self.__sell = 5
+    elif self.__sell > 0:
+      self.__sell -= 1
+      if self.__sell == 0:
+        self.__fund.append(data)
+        self.__buy = 2
+    elif self.__buy > 0:
+      self.__buy -= 1
+      if self.__buy == 0:
+        self.__fund.append(data)
+        self.prices.append(self.__fund)
+
+class AiaFund():
+  __price = []
+  def getinfo(self):
+#req = urllib2.Request(url='https://direct.aia.co.th/btob/FundQueryServlet?actionType=show',
+#data='This data is passed to stdin of the CGI')
+#f = urllib2.urlopen(req)
+    f = open('fund.html', 'r')
+    parser = MyHTMLParser()
+    parser.feed(f.read())
+    self.__price = parser.prices
+    return self.__price
+  def show(self):
+    p = self.getinfo()
+    for f in p:
       print f[0], f[1], f[2]
 
+  def json(self):
+    p = self.getinfo()
+    j = json.dumps(p)
+    return j
 
-req = urllib2.Request(url='https://direct.aia.co.th/btob/FundQueryServlet?actionType=show',
-    data='This data is passed to stdin of the CGI')
-f = urllib2.urlopen(req)
 
-#f = open('fund.html', 'r')
+def main():
+  a = AiaFund()
+  a.show()
 
-parser = MyHTMLParser()
-parser.feed(f.read())
-parser.pricelist()
+if __name__ == '__main__':
+  main()
 
