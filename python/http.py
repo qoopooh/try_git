f6f0e257ae185198f105f4666303288f18128e2b
Berm Mini <mac@mini.berm>
2013-09-26 22:42:18 +0700

    try to get the real one

--- python/http.py
+++ python/http.py
@@ -6,7 +6,11 @@ import types
 
 from HTMLParser import HTMLParser
 
+
 class MyHTMLParser(HTMLParser):
+  sell, buy = 0, 0
+  prices, fund = [], []
+
   def handle_starttag(self, tag, attrs):
 #print 'start ', tag
     return
@@ -15,16 +19,31 @@ class MyHTMLParser(HTMLParser):
     return
   def handle_data(self, data):
     u = unicode(data, 'tis-620')
-    if u'กอง' in u:
-      print 'dat ', u
+    if u'กองทุนเปิด' in u:
+      self.fund = []
+      self.fund.append(u)
+      self.sell = 5
+    elif self.sell > 0:
+      self.sell -= 1
+      if self.sell == 0:
+        self.fund.append(data)
+        self.buy = 2
+    elif self.buy > 0:
+      self.buy -= 1
+      if self.buy == 0:
+        self.fund.append(data)
+        self.prices.append(self.fund)
+  def pricelist(self):
+    for f in self.prices:
+      print f[0], f[1], f[2]
 
-#req = urllib2.Request(url='https://direct.aia.co.th/btob/FundQueryServlet?actionType=show',
-#data='This data is passed to stdin of the CGI')
-#f = urllib2.urlopen(req)
 
-f = open('fund.html', 'r')
+req = urllib2.Request(url='https://direct.aia.co.th/btob/FundQueryServlet?actionType=show', data='This data is passed to stdin of the CGI')
+f = urllib2.urlopen(req)
 
-#print f.read()
+#f = open('fund.html', 'r')
+
 parser = MyHTMLParser()
 parser.feed(f.read())
+parser.pricelist()
 
