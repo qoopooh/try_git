9e962fd06337bb496e5d1b3d4b73822ba75b97b4
phuchit aae <phuchit@aae.co.th>
2013-09-27 10:53:38 +0700

    STEP 13 : Adding constraint

--- python/app.py
+++ python/app.py
@@ -10,10 +10,25 @@ class simpleapp(wx.Frame):
     self.initialize()
 
   def initialize(self):
+    sizer = wx.GridBagSizer()
+    self.entry = wx.TextCtrl(self, -1, value=u'ใส่ข้อความตรงนี้')
+    sizer.Add(self.entry, (0,0), (1,1), wx.EXPAND)
+
+    button = wx.Button(self,-1,label=u'กดฉัน')
+    sizer.Add(button, (0,1))
+
+    self.label = wx.StaticText(self,-1,label=u'สวัสดี')
+    self.label.SetBackgroundColour(wx.BLUE)
+    self.label.SetForegroundColour(wx.GREEN)
+    sizer.Add(self.label, (1,0), (1,2), wx.EXPAND)
+
+    sizer.AddGrowableCol(0)
+    self.SetSizerAndFit(sizer)
+    self.SetSizeHints(self.GetSize().x, self.GetSize().y, -1, self.GetSize().y)
     self.Show(True)
 
 if __name__=="__main__":
   app = wx.App()
-  frame = simpleapp(None, -1, 'my application')
+  frame = simpleapp(None, -1, u'ทดสอบ')
   app.MainLoop()
 
