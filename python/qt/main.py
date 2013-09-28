1619684a2b40164bbcc577a72efa036e046b671a
pooh arch64 <pooh@arch64.n570>
2013-09-29 01:26:05 +0700

    button and tooltips

--- python/qt/main.py
+++ python/qt/main.py
@@ -15,6 +15,14 @@ class Example(QtGui.QMainWindow):
         self.initUI()
 
     def initUI(self):
+        QtGui.QToolTip.setFont(QtGui.QFont('SansSerif', 10))
+        self.setToolTip('This is a <b>QWidget</b> widget')
+
+        btn = QtGui.QPushButton('Button', self)
+        btn.setToolTip('This is a <b>QPushButton</b> widget')
+        btn.resize(btn.sizeHint())
+        btn.move(50, 50)
+
         self.setGeometry(300, 300, 250, 150)
         self.setWindowTitle('Icon')
         self.setWindowIcon(QtGui.QIcon('ksig.png'))
