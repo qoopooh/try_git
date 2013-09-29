4ce3f5b8c909414a95d73e30b3be71775f62a44a
pooh arch64 <pooh@arch64.n570>
2013-09-29 23:47:42 +0700

    done

--- python/kivy/paint.py
+++ python/kivy/paint.py
@@ -4,22 +4,39 @@
 Test kivy
 http://kivy.org/docs/tutorials/firstwidget.html
 """
+from random import random
 from kivy.app import App
 from kivy.uix.widget import Widget
-from kivy.graphics import Color, Ellipse
+from kivy.uix.button import Button
+from kivy.graphics import Color, Ellipse, Line
 
 class PaintWidget(Widget):
 
     def on_touch_down(self, touch):
+        color = (random(), random(), random())
         with self.canvas:
-            Color(1, 1, 0)
-            d = 30.
+            Color(*color, mode='hsv')
+            d = 30
             Ellipse(pos=(touch.x - d / 2, touch.y - d / 2), size=(d, d))
+            touch.ud['line'] = Line(points=(touch.x, touch.y))
+
+    def on_touch_move(self, touch):
+        touch.ud['line'].points += [touch.x, touch.y]
 
 class PaintApp(App):
 
     def build(self):
-        return PaintWidget()
+        parent = Widget()
+        painter = PaintWidget()
+        clearbtn = Button(text='Clear')
+        parent.add_widget(painter)
+        parent.add_widget(clearbtn)
+
+        def clear_canvas(obj):
+            painter.canvas.clear()
+        clearbtn.bind(on_release=clear_canvas)
+
+        return parent
 
 def main():
     PaintApp().run()
