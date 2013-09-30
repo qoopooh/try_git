625989d18e7590e36df3529917adb48b4c860edb
Berm AAE015 sid <sid@aae015>
2013-09-29 21:56:37 -0700

    show kivy thai font on sid

---
+++ python/kivy/aae/log.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Log from reader
+"""
+from kivy import resources
+from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+#from kivy.core.text.markup import MarkupLabel
+
+class LogScreen(GridLayout):
+    def __init__(self, **kwargs):
+        super(LogScreen, self).__init__(**kwargs)
+
+        resources.resource_add_path('/usr/share/fonts/truetype/droid/')
+        self.cols = 2
+        self.add_widget(Label(text=u'ชื่อผู้ใช้งาน',font_name='DroidSansThai.ttf'))
+        self.username = TextInput(multiline=False)
+        self.add_widget(self.username)
+        self.add_widget(Label(text=u'รหัสผ่าน',font_name='DroidSansThai.ttf'))
+        self.password = TextInput(password=True, multiline=False)
+        self.add_widget(self.password)
+
+class TestApp(App):
+    def build(self):
+        return LogScreen()
+
+def main():
+    TestApp().run()
+
+if __name__ == '__main__':
+    main()
+
