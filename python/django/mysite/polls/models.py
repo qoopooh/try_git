c5b4acef6f043027286fbe38e9f157724df2dfd3
pooh arch64 <pooh@arch64.n570>
2013-10-01 22:28:02 +0700

    cp /usr/lib/python2.7/site-packages/django/contrib/admin/templates/admin/base_site.html mysite/templates/

--- python/django/mysite/polls/models.py
+++ python/django/mysite/polls/models.py
@@ -1,4 +1,6 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Poll(models.Model):
@@ -10,6 +12,9 @@ class Poll(models.Model):
 
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
 
 class Choice(models.Model):
     poll = models.ForeignKey(Poll)
