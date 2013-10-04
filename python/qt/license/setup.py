e6539cccc1beab88b742a0b2ea69c29ad5a349ef
phuchit aae <phuchit@aae.co.th>
2013-10-04 08:30:57 +0700

    py2exe
    as http://www.py2exe.org/index.cgi/Tutorial
    - download Microsoft Visual C++ 2008 Redistributable Package (x86)
    http://www.microsoft.com/en-us/download/confirmation.aspx?id=29
    - py2exe-0.6.9.win32-py2.7.exe
    http://downloads.sourceforge.net/project/py2exe/py2exe/0.6.9/py2exe-0.6.9.win32-py2.7.exe?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fpy2exe%2Ffiles%2Fpy2exe%2F0.6.9%2F&ts=1380850248&use_mirror=jaist

---
+++ python/qt/license/setup.py
@@ -0,0 +1,8 @@
+import sys
+
+from distutils.core import setup
+import py2exe
+
+sys.path.append("C:\\Program Files\\Microsoft Visual Studio 9.0\\VC\\redist\\x86\\Microsoft.VC90.CRT")
+setup(console=['licence.py'])
+
