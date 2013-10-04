583fcdd71d163b344b65f3cd7e8f89dcfa34b98e
phuchit aae <phuchit@aae.co.th>
2013-10-04 12:38:24 +0700

    gen ui

--- python/qt/license/setup.py
+++ python/qt/license/setup.py
@@ -1,9 +1,10 @@
-import sys
+import sys, subprocess
 
 from distutils.core import setup
 import py2exe
 
 sys.path.append("C:\\Program Files\\Microsoft Visual Studio 9.0\\VC\\redist\\x86\\Microsoft.VC90.CRT")
+subprocess.call(['pyside-uic', 'licence.ui', '-o', 'ui_licence.py'])
 
 setup(
     windows=['licence.py'],
