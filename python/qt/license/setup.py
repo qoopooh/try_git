a2e343c19f584a87f0b4b2e7151d0f51e6699bdf
phuchit aae <phuchit@aae.co.th>
2013-10-04 09:28:29 +0700

    include others to setup.py

--- python/qt/license/setup.py
+++ python/qt/license/setup.py
@@ -4,5 +4,13 @@ from distutils.core import setup
 import py2exe
 
 sys.path.append("C:\\Program Files\\Microsoft Visual Studio 9.0\\VC\\redist\\x86\\Microsoft.VC90.CRT")
-setup(console=['licence.py'])
+
+setup(
+    windows=['licence.py'],
+    data_files= [
+        ('', ['exit.png']),
+        ('', ['kpdf.png']),
+        ('', ['COPYING.txt']),
+    ]
+)
 
