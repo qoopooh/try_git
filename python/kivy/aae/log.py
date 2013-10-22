f2fb03e41ba2c8908da4b7cfd79281e6a25667c7
Berm Mini <mac@mini.berm>
2013-10-22 23:46:54 +0700

    kivy thai font on mac

--- python/kivy/aae/log.py
+++ python/kivy/aae/log.py
@@ -3,6 +3,8 @@
 """
 Log from reader
 """
+import os
+
 from kivy import resources
 from kivy.app import App
 from kivy.uix.button import Button
@@ -14,11 +16,17 @@ class LogScreen(GridLayout):
     def __init__(self, **kwargs):
         super(LogScreen, self).__init__(**kwargs)
 
-        resources.resource_add_path('/usr/share/fonts/truetype/droid/')
+        platform = os.sys.platform
+        if platform == 'linux2':
+            resources.resource_add_path('/usr/share/fonts/truetype/droid/')
+        elif platform == 'darwin':
+            resources.resource_add_path('/Library/Fonts')
         self.cols = 2
+#self.add_widget(Label(text=u'User',font_name='DroidSans.ttf'))
         self.add_widget(Label(text=u'ชื่อผู้ใช้งาน',font_name='DroidSansThai.ttf'))
         self.username = TextInput(multiline=False)
         self.add_widget(self.username)
+#self.add_widget(Label(text=u'Passwd',font_name='DroidSans.ttf'))
         self.add_widget(Label(text=u'รหัสผ่าน',font_name='DroidSansThai.ttf'))
         self.password = TextInput(password=True, multiline=False)
         self.add_widget(self.password)
