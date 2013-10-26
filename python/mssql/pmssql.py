a25035e92fd470fb93908d4f13347b0c62510877
pooh arch64 <pooh@arch64.n570>
2013-10-26 23:19:13 +0700

    on arch64 getstart does not work, but pmssql can talk with COMTHAI

--- python/mssql/pmssql.py
+++ python/mssql/pmssql.py
@@ -2,10 +2,15 @@
 
 import pymssql
 
-conn = pymssql.connect(host='aaebio\\bsserver', user='sa', password='sa', database='domotics')
+#conn = pymssql.connect(host='aaebio\\bsserver', user='sa', password='sa', database='domotics')
+conn = pymssql.connect(host='192.168.1.152', user='sa', password='sa', database='EUROSOFT')
 cur = conn.cursor()
+#cur.execute("""
+    #SELECT * FROM command
+    #""")
 cur.execute("""
-    SELECT * FROM command
+    SELECT * FROM tblConfig
+    WHERE Config_Value = 'false'
     """)
 row=cur.fetchone()
 while row:
