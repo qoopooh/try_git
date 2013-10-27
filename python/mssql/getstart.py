95cc60296c041eb27c632bf7351da56f0192c084
Berm Mini <mac@mini.berm>
2013-10-27 12:20:26 +0700

    separate query

--- python/mssql/getstart.py
+++ python/mssql/getstart.py
@@ -3,22 +3,27 @@
 import pyodbc
 import json
 import collections
+import eurosoft
 
 cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER=COMTHAI;DATABASE=EUROSOFT;UID=sa;PWD=sa')
 cursor = cnxn.cursor();
-cursor.execute("""
-    SELECT * FROM tblConfig
-    WHERE Config_Value = 'false'
-    """)
+#cursor.execute("""
+    #SELECT * FROM tblConfig
+    #WHERE Config_Value = 'false'
+    #""")
+cursor.execute(eurosoft.WIT_NTO)
 
 rows = cursor.fetchall()
 
 rowarray_list = []
-for row in rows:
-  t = (row.Config_ID, row.Config_Name)
-  rowarray_list.append(t)
+for r in rows:
+#t = (row.Config_ID, row.Config_Name)
+#t = (row.Tyre_SerialNo, row.Size_Name, row.Model_Name)
+    t = (r[0], r[1])
+    rowarray_list.append(t)
 
-j = json.dumps(rowarray_list)
+j = json.dumps(rowarray_list, ensure_ascii=False, indent=2).encode("utf8")
+#print j
 rowarray_file = 'output.js'
 f = open(rowarray_file, 'w')
 print >> f, j
--- python/mssql/pmssql.py
+++ python/mssql/pmssql.py
@@ -1,37 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import sys, pymssql
-
-S_REJ_DET = """ SELECT * FROM tblRejectTransactionDetail
-                WHERE RejectTransDetail_IsConfirm = 1 """
-S_PRO_DET = """ SELECT * FROM tblProductionTransactionDetail
-                WHERE ProdTransDetail_IsConfirm != 1 OR 
-                    ProdTransDetail_IsAuthorise != 1"""
-S_PRO_DETN = """ SELECT * FROM tblProductionTransactionDetail
-                WHERE ProdTransDetail_IsAuthorise != 1"""
-WIT_NTO = """
-SELECT TOP 100 NewTransDetail_NewTrans_ID, Comp_Name
-FROM tblNewTyreTransactionDetail, tblDocument, tblCompany
-WHERE NewTransDetail_IsConfirm = 1
-    AND NewTransDetail_NewTrans_ID=Doc_Transaction_ID
-    AND Doc_Comp_ID=Comp_ID
-    """
-WIT_NTO_ID = """
-SELECT Tyre_SerialNo,Size_Name,Model_Name
-FROM tblNewTyreTransactionDetail,tblTyre,tblSize,tblModel
-WHERE NewTransDetail_Tyre_Serial=Tyre_SerialNo
-    AND NewTransDetail_Tyre_Code=Tyre_Code
-    AND Tyre_Size_ID=Size_ID
-    AND Tyre_Model_ID=Model_ID
-    AND NewTransDetail_NewTrans_ID LIKE 'NTO13100016%'
-    """
+import sys, pymssql, eurosoft
 
 #conn = pymssql.connect(host='aaebio\\bsserver', user='sa', password='sa', database='domotics')
 conn = pymssql.connect(host='192.168.1.152', user='sa', password='sa',
         database='EUROSOFT', charset='utf8')
 cur = conn.cursor()
-cur.execute(WIT_NTO)
+cur.execute(eurosoft.WIT_NTO)
 row=cur.fetchone()
 count = 0
 while row:
