9ce4214a10edb1824eac91e1fe96d2eadafe0fe7
Berm Mini <mac@mini.berm>
2013-10-28 01:36:08 +0700

    sleep

--- python/mssql/eurosoft.py
+++ python/mssql/eurosoft.py
@@ -6,14 +6,14 @@ S_PRO_DET = """ SELECT * FROM tblProductionTransactionDetail
                     ProdTransDetail_IsAuthorise != 1"""
 S_PRO_DETN = """ SELECT * FROM tblProductionTransactionDetail
                 WHERE ProdTransDetail_IsAuthorise != 1"""
-WIT_NTO = """
+WIT_NT = """
 SELECT TOP 100 NewTransDetail_NewTrans_ID, Comp_Name
 FROM tblNewTyreTransactionDetail, tblDocument, tblCompany
 WHERE NewTransDetail_IsConfirm = 1
     AND NewTransDetail_NewTrans_ID=Doc_Transaction_ID
     AND Doc_Comp_ID=Comp_ID
     """
-WIT_NTO_ID = """
+WIT_NT_ID = """
 SELECT Tyre_SerialNo,Size_Name,Model_Name
 FROM tblNewTyreTransactionDetail,tblTyre,tblSize,tblModel
 WHERE NewTransDetail_Tyre_Serial=Tyre_SerialNo
@@ -21,5 +21,24 @@ WHERE NewTransDetail_Tyre_Serial=Tyre_SerialNo
     AND Tyre_Size_ID=Size_ID
     AND Tyre_Model_ID=Model_ID
     AND NewTransDetail_NewTrans_ID = '{id}'
+    """
+
+WIT_CUS = """
+SELECT ProdTransDetail_ProdTrans_ID, Comp_Name
+FROM tblProductionTransactionDetail, tblCasing, tblCompany
+WHERE ProdTransDetail_Casing_ID=Casing_ID
+    AND Casing_Owner_ID=Comp_ID
+    AND ProdTransDetail_IsConfirm != 1
+    """
+WIT_CUS_ID = """
+SELECT Tyre_SerialNo,Size_Name,Model_Name
+FROM tblProductionTransactionDetail,tblCasing,tblTyre,tblSize,tblModel
+WHERE ProdTransDetail_Casing_ID=Casing_ID
+    AND Casing_OwnerBranch_ID>0
+    AND Casing_Tyre_Serial=Tyre_SerialNo
+    AND Casing_Tyre_Code=Tyre_Code
+    AND Tyre_Size_ID=Size_ID
+    AND Tyre_Model_ID=Model_ID
+    AND ProdTransDetail_ProdTrans_ID='{id}'
     """
 
--- python/mssql/getstart.py
+++ python/mssql/getstart.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 import pyodbc
 import json
@@ -8,20 +9,26 @@ import eurosoft
 #cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER=COMTHAI;DATABASE=EUROSOFT;UID=sa;PWD=sa')
 cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER=192.168.1.152;DATABASE=EUROSOFT;UID=sa;PWD=sa')
 cursor = cnxn.cursor();
-query = eurosoft.WIT_NTO_ID.format(id='NTO13100015')
+
+query = eurosoft.WIT_CUS
+#query = eurosoft.WIT_NT_ID.format(id='NTO13100015')
+#query = eurosoft.WIT_CUS_ID.format(id='PRI13100011')
 #print query
 cursor.execute(query)
 
 rows = cursor.fetchall()
 
 rowarray_list = []
+count = 0
 for r in rows:
 #t = (row.Tyre_SerialNo, row.Size_Name, row.Model_Name)
     t = (r[0], r[1])
     rowarray_list.append(t)
+    count += 1
 
 j = json.dumps(rowarray_list, ensure_ascii=False, indent=2).encode("utf8")
-print j
+#print j
+print 'นับ', count
 rowarray_file = 'output.js'
 f = open(rowarray_file, 'w')
 print >> f, j
