62371f4481d51220319189bae0da331c4d155e12
phuchit aae <phuchit@aae.co.th>
2013-10-28 08:54:07 +0700

    finish sample

--- python/jython/sw/click/simple.py
+++ python/jython/sw/click/simple.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env jython
 # -*- coding: utf-8 -*-
 
-from javax.swing import JFrame
+import sys
+from javax.swing import JFrame, JPanel, JButton
 
 class Example(JFrame):
 
@@ -11,11 +12,29 @@ class Example(JFrame):
         self.initUI()
 
     def initUI(self):
+
+        panel = JPanel()
+        panel.setLayout(None)
+        panel.setToolTipText('A Panel container')
+
+        button = JButton('Click')
+        button.setBounds(120, 60, 100, 30)
+        button.setToolTipText('A button component')
+        panel.add(button)
+
+        qbutton = JButton('Quit', actionPerformed=self.onQuit)
+        qbutton.setBounds(10, 60, 80, 30)
+        panel.add(qbutton)
+
+        self.getContentPane().add(panel)
         self.setTitle('Simple')
         self.setSize(250, 200)
         self.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE)
         self.setLocationRelativeTo(None)
         self.setVisible(True)
+
+    def onQuit(self, e):
+        sys.exit(0)
 
 if __name__ == '__main__':
     Example()
