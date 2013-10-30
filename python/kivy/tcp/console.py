094979e936c8e54d6a3ebc245a32f821164836b9
phuchit aae <phuchit@aae.co.th>
2013-10-30 10:39:23 +0700

    copy to android

--- python/kivy/tcp/console.py
+++ python/kivy/tcp/console.py
@@ -5,6 +5,10 @@ Builder.load_file('console.kv')
 
 class Console(BoxLayout):
 
+    def __init__(self, app, **kwargs):
+        super(Console, self).__init__(**kwargs)
+        self._app = app
+
     def text(self, val):
         print('text input text is: {txt}'.format(txt=val))
 
--- python/kivy/tcp/main.py
+++ python/kivy/tcp/main.py
@@ -6,8 +6,55 @@ from console import Console
 
 class ControllerApp(App):
 
+    use_kivy_settings = False
+    #icon = 'Logo_only.ico'
+
+    def build_config(self, config):
+        config.setdefaults('section1', {
+            'ip': '192.168.1.31',
+            'port': 1470,
+            'format': 'HEX',
+            'newline': 'CRLF',
+        })
+
+    def build_settings(self, settings):
+        jsondata = """
+            [{ "type": "title", "title": "Socket Settings" },
+            { "type": "string",
+                "title": "Server IP",
+                "desc": "IP address of TCP server",
+                "section": "section1",
+                "key": "ip" },
+            { "type": "numeric",
+                "title": "Server Port",
+                "desc": "TCP socket port",
+                "section": "section1",
+                "key": "port" },
+            { "type": "options",
+                "title": "Format",
+                "desc": "Data communication format",
+                "section": "section1",
+                "options": ["HEX", "ASCII"],
+                "key": "format" },
+            { "type": "options",
+                "title": "New Line",
+                "desc": "New line splitter for ASCII format",
+                "section": "section1",
+                "options": ["CR", "LF", "CRLF"],
+                "key": "newline" }
+            ]"""
+
+        settings.add_json_panel('TCP Client',
+                self.config, data=jsondata)
+
+    def on_config_change(self, config, section, key, value):
+        if config is self.config:
+            token = (section, key)
+            if token == ('section1', 'ip'):
+                print('Server IP has been change to', value)
+
     def build(self):
-        return Console(info='Hello World')
+        return Console(self)
 
 if __name__ == '__main__':
     ControllerApp().run()
