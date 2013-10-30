1885f61b70b668085610bc34d8fb7b5892d9dd82
phuchit aae <phuchit@aae.co.th>
2013-10-30 16:09:51 +0700

    read hex

--- python/kivy/tcp/client.py
+++ python/kivy/tcp/client.py
@@ -6,7 +6,10 @@ from socket import socket, AF_INET, SOCK_STREAM
 from threading import Thread
 from datetime import datetime
 
-class Client():
+from sthex import *
+
+
+class Client(object):
 
     def __init__(self, recv_cb=None):
         self._recv_cb = recv_cb
@@ -14,13 +17,37 @@ class Client():
         self._host = '192.168.1.92'
         self._port = 1470
         self._connect = False
+        self._rx_hex = True
+        self._tx_hex = True
 
     @property
     def connecting(self):
         return self._connect
 
+    @property
+    def rx_hex(self):
+        return self._rx_hex
+    @rx_hex.setter
+    def rx_hex(self, val):
+        self._rx_hex = val
+
+    @property
+    def tx_hex(self):
+        return self._tx_hex
+    @rx_hex.setter
+    def tx_hex(self, val):
+        self._tx_hex = val
+
+    @property
+    def newline(self):
+        r""" Return '\r', '\n' and '\r\n' """
+        return self._nl
+    @newline.setter
+    def newline(self, val):
+        self._nl = val
+
     def reconnect(self, ip=None, port=None):
-        self._s.close()
+        self.close()
         self.connect(ip, port)
 
     def connect(self, ip=None, port=None):
@@ -42,6 +69,8 @@ class Client():
         chunk = self._s.recv(1024)
         while len(chunk) > 0:
             if self._recv_cb is not None:
+                if self._rx_hex:
+                    chunk = hex_to_hexstr(chunk)
                 self._recv_cb(chunk)
             chunk = self._s.recv(1024)
         self.close()
@@ -49,11 +78,11 @@ class Client():
     def send(self, data):
         sz = 0
         if self._connect:
-            sz = self._s.send(data)
+            if self._tx_hex:
+                sz = self._s.send(bytearray(hexstr_to_hex(data)))
+            else:
+                sz = self._s.send(data)
         return sz
-
-    def __del__(self):
-        self._s.close()
 
 def print_data(data):
     print datetime.now(), len(data), data
@@ -61,11 +90,14 @@ def print_data(data):
 if __name__ == '__main__':
     c = Client(print_data)
     c.connect()
-    sz = c.send('Hi')
+    sz = c.send('486920')
     count = 0
     while sz > 0:
         time.sleep(1)
-        sz = c.send(' {c}'.format(c=count))
-        count += 1
-        pass
+#sz = c.send(' {c}'.format(c=count))
+        sz = c.send('203{c}'.format(c=count))
+        if count < 9:
+            count += 1
+        else:
+            count = 0
 
---
+++ python/kivy/tcp/sthex.py
@@ -0,0 +1,24 @@
+__all__ = ['str_to_hex', 'hexstr_to_hex', 'hex_to_hexstr', 'hex_to_str']
+
+class HexToStringError(Exception):
+    pass
+
+def str_to_hex(s):
+    return map(ord, s)
+
+def hexstr_to_hex(s):
+    return map(ord, s.decode("hex"))
+
+def hex_to_hexstr(l):
+    h = str_to_hex(l)
+    return ''.join('{0:02x}'.format(b) for b in h)
+
+def hex_to_str(l):
+    s = ''
+    for i in l:
+        if 31 < i < 127:
+            s += chr(i)
+        else:
+            raise HexToStringError()
+    return s
+
