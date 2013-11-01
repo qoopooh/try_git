88d3afe7c744558ed4a39d291bca7f5b0a2084db
Berm Mini <mac@mini.berm>
2013-11-01 23:21:02 +0700

    copy from svn

--- python/mssql/eurosoft.py
+++ python/mssql/eurosoft.py
@@ -67,9 +67,118 @@ ORDER BY ProdTrans_Create_Date DESC
 WIT_STO_ID = WIT_CUS_ID
 
 WIT_REJ = """
+SELECT RejectTransDetail_RejectTrans_ID,Comp_Name
+FROM tblRejectTransactionDetail, tblCasing,tblCompany
+WHERE RejectTransDetail_Casing_ID=Casing_ID
+    AND Casing_Owner_ID=Comp_ID
 """
 
 WIT_REJ_ID = """
+SELECT Tyre_SerialNo,Size_Name,Model_Name
+FROM tblRejectTransactionDetail,tblCasing,tblTyre,tblSize,tblModel
+WHERE RejectTransDetail_Casing_ID=Casing_ID
+    AND Casing_Tyre_Serial=Tyre_SerialNo
+    AND Casing_Tyre_Code=Tyre_Code
+    AND Tyre_Size_ID=Size_ID
+    AND Tyre_Model_ID=Model_ID
+    AND RejectTransDetail_RejectTrans_ID='{tid}'
 """
+
+INV_NT = """
+SELECT Size_Name, COUNT(Size_ID)
+FROM tblNewTyreStock,tblTyre,tblSize
+WHERE NewStock_Tyre_Serial=Tyre_SerialNo
+    AND NewStock_Tyre_Code=Tyre_Code
+    AND Tyre_Size_ID=Size_ID
+GROUP BY Size_ID
+"""
+
+INV_NT_ID = """
+SELECT NewStock_Tyre_Serial,Model_Name
+FROM tblNewTyreStock,tblTyre,tblSize,tblModel
+WHERE NewStock_Tyre_Serial=Tyre_SerialNo
+    AND NewStock_Tyre_Code=Tyre_Code
+    AND Tyre_Size_ID=Size_ID
+    AND Tyre_Model_ID=Model_ID
+    AND Size_Name='{sz}'
+"""
+
+INV_CUS = """
+SELECT Size_Name,COUNT(Size_ID)
+FROM tblProductionStock,tblCasing,tblTyre,tblSize
+WHERE ProdStock_Casing_ID=Casing_ID
+    AND Casing_Tyre_Serial=Tyre_SerialNo
+    AND Casing_Tyre_Code=Tyre_Code
+    AND Casing_OwnerBranch_ID>0
+    AND Tyre_Size_ID=Size_ID
+GROUP BY Size_ID
+"""
+
+INV_CUS_ID = """
+SELECT Casing_Tyre_Serial,Model_Name
+FROM tblProductionStock,tblCasing,tblTyre,tblSize,tblModel
+WHERE ProdStock_Casing_ID=Casing_ID
+    AND Casing_Tyre_Serial=Tyre_SerialNo
+    AND Casing_Tyre_Code=Tyre_Code
+    AND Casing_OwnerBranch_ID>0
+    AND Tyre_Size_ID=Size_ID
+    AND Tyre_Model_ID=Model_ID
+    AND Size_Name='{sz}'
+"""
+
+INV_STO = """
+SELECT Size_Name,COUNT(Size_ID)
+FROM tblProductionStock,tblCasing,tblTyre,tblSize
+WHERE ProdStock_Casing_ID=Casing_ID
+    AND Casing_Tyre_Serial=Tyre_SerialNo
+    AND Casing_Tyre_Code=Tyre_Code
+    AND Casing_OwnerBranch_ID=-1
+    AND Tyre_Size_ID=Size_ID
+GROUP BY Size_ID
+"""
+
+INV_STO_ID = """
+SELECT Casing_Tyre_Serial,Model_Name
+FROM tblProductionStock,tblCasing,tblTyre,tblSize,tblModel
+WHERE ProdStock_Casing_ID=Casing_ID
+    AND Casing_Tyre_Serial=Tyre_SerialNo
+    AND Casing_Tyre_Code=Tyre_Code
+    AND Casing_OwnerBranch_ID=-1
+    AND Tyre_Size_ID=Size_ID
+    AND Tyre_Model_ID=Model_ID
+    AND Size_Name='{sz}'
+"""
+
+INV_REJ = """
+SELECT Size_Name,COUNT(Size_ID)
+FROM tblRejectStock,tblCasing,tblTyre,tblSize
+WHERE RejectStock_Casing_ID=Casing_ID
+    AND Casing_Tyre_Serial=Tyre_SerialNo
+    AND Casing_Tyre_Code=Tyre_Code
+    AND Tyre_Size_ID=Size_ID
+GROUP BY Size_ID
+"""
+
+INV_REJ_ID = """
+SELECT Casing_Tyre_Serial,Model_Name
+FROM tblRejectStock,tblCasing,tblTyre,tblSize,tblModel
+WHERE RejectStock_Casing_ID=Casing_ID
+    AND Casing_Tyre_Serial=Tyre_SerialNo
+    AND Casing_Tyre_Code=Tyre_Code
+    AND Tyre_Size_ID=Size_ID
+    AND Tyre_Model_ID=Model_ID
+    AND Size_Name='{sz}'
+"""
+
+IDENTIFY = """
+SELECT Tyre_SerialNo, Tyre_Code, Size_Name, B
+, Model_Name
+FROM tblTyre, tblSize, tblBrand, tblModel
+WHERE Tyre_Size_ID=Size_ID
+    AND Tyre_Brand_ID=Brand_ID
+    AND Tyre_Model_ID=Model_ID
+    AND Tyre_SerialNo='{sn}'
+"""
+
 
 
