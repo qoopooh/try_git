33386b7d179ba19361fc45ec3b8a7018922aee7d
Berm Mini <mac@mini.berm>
2013-11-05 00:37:49 +0700

    query [] on pymssql/wheezy

--- python/mssql/pmssql.py
+++ python/mssql/pmssql.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import sys, pymssql, eurosoft
+import sys, pymssql
+from eurosoft import *
 
 #conn = pymssql.connect(host='aaebio\\bsserver', user='sa', password='sa', database='domotics')
-conn = pymssql.connect(host='192.168.1.152', user='sa', password='sa',
+conn = pymssql.connect(host='192.168.1.153', user='sa', password='sa',
         database='EUROSOFT', charset='utf8')
 cur = conn.cursor()
-cur.execute(eurosoft.WIT_NTO)
-row=cur.fetchone()
+#query = WIT_STO
+query = IDENTIFY.format(sn='CVO46179H')
+cur.execute(query)
+#row=cur.fetchone()
+rows=cur.fetchall()
+print query, rows
 count = 0
-while row:
+#while row:
+for row in rows:
     print row[0], row[1]
     row = cur.fetchone()
     count += 1
