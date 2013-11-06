b62e8c7f7e881cc8027a14fec0a3cd6d38b7132a
Berm Aae014 <phuchit.aae@gmail.com>
2013-11-06 14:39:56 +0700

    can put 20131106, but it can show nobody

--- python/mssql/date.py
+++ python/mssql/date.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python
 
-from datetime import date, timedelta
+from datetime import date, timedelta, datetime
 from calendar import timegm
+from re import compile
+
+r = compile('\d{8}')
 
 def gmt(d='today'):
+    tm = date.today()
     if d == 'yesterday':
-        tm = date.today() - timedelta(1)
-    else:
-        tm = date.today()
+        tm -= timedelta(1)
+    elif len(d) == 8:
+        if r.match(d) is not None:
+            tm = datetime.strptime(d, '%Y%m%d')
     cal = timegm(tm.timetuple())
     return cal
 
@@ -15,5 +20,7 @@ if __name__ == '__main__':
     i = gmt('yesterday')
     print i
     i = gmt('today')
+    print i
+    i = gmt('20131104')
     print i
 
