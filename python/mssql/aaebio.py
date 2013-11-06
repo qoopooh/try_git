d7ba5510c8d8deac5bbe0a1a1ed425f82a41d447
Berm Aae014 <phuchit.aae@gmail.com>
2013-11-06 11:57:35 +0700

    GET is ok

--- python/mssql/aaebio.py
+++ python/mssql/aaebio.py
@@ -15,6 +15,24 @@ FROM TB_TA_RESULT INNER JOIN
 WHERE (TB_TA_RESULT.nDateTime = {date})
 ORDER BY TB_USER_DEPT.sDepartment, CONVERT(INT, TB_USER.sUserID)
 """
+PRODUCTION_REPORT = """
+SELECT TB_TA_RESULT.nStartTime, TB_TA_RESULT.nEndTime, TB_USER.sUserID, TB_USER.sUserName, TB_USER_DEPT.sDepartment
+FROM TB_TA_RESULT INNER JOIN
+    TB_USER ON TB_TA_RESULT.nUserIdn = TB_USER.nUserIdn LEFT OUTER JOIN
+    TB_USER_DEPT ON TB_USER.nDepartmentIdn = TB_USER_DEPT.nDepartmentIdn
+WHERE TB_TA_RESULT.nDateTime = {date}
+    AND TB_USER_DEPT.sDepartment='Production'
+ORDER BY TB_USER_DEPT.sDepartment, CONVERT(INT, TB_USER.sUserID)
+"""
+ADMIN_REPORT = """
+SELECT TB_TA_RESULT.nStartTime, TB_TA_RESULT.nEndTime, TB_USER.sUserID, TB_USER.sUserName, TB_USER_DEPT.sDepartment
+FROM TB_TA_RESULT INNER JOIN
+    TB_USER ON TB_TA_RESULT.nUserIdn = TB_USER.nUserIdn LEFT OUTER JOIN
+    TB_USER_DEPT ON TB_USER.nDepartmentIdn = TB_USER_DEPT.nDepartmentIdn
+WHERE TB_TA_RESULT.nDateTime = {date}
+    AND TB_USER_DEPT.sDepartment<>'Production'
+ORDER BY TB_USER_DEPT.sDepartment, CONVERT(INT, TB_USER.sUserID)
+"""
 OUTPUT_FILE = 'output.js'
 
 def work_time(s, e):
@@ -29,24 +47,29 @@ def work_time(s, e):
         return 0
     return w-60
 
-def time_report(d=0):
+def time_report(dt=0,department='production'):
 
     count = 0
     rowarray_list = []
 
-    if d < 1:
+    if dt < 1:
         return rowarray_list
 
-    query = TIME_REPORT.format(date=d)
     conn = pymssql.connect(host='aaebio\\bsserver', user='sa', password='sa',
             database='BioStar', as_dict=True)
     cur = conn.cursor()
+    if department == 'production':
+        query = PRODUCTION_REPORT.format(date=dt)
+    elif department == 'admin':
+        query = ADMIN_REPORT.format(date=dt)
+    else:
+        query = TIME_REPORT.format(date=dt)
     cur.execute(query)
     rows=cur.fetchall()
     conn.close()
 
     for row in rows:
-        date = strftime("%Y-%m-%d", localtime(d))
+        date = strftime("%Y-%m-%d", localtime(dt))
         start_time = strftime("%M:%S", localtime(row['nStartTime']))
         end_time = strftime("%M:%S", localtime(row['nEndTime']))
         work = strftime("%M:%S", \
@@ -61,8 +84,8 @@ def time_report(d=0):
 
     return rowarray_list
 
-def create_json():
-    report = time_report(gmt('yesterday'))
+def create_json(date, department):
+    report = time_report(gmt(date), department)
     j = json.dumps(report, ensure_ascii=False, indent=2).encode("utf8")
     f = open(OUTPUT_FILE, 'w')
     f.write(j)
@@ -71,11 +94,20 @@ def create_json():
 
 class index:
     def GET(self):
-        return create_json()
+        i = web.input()
+        return render.index(i.name)
+
+class TimeReport:
+    def GET(self):
+        i = web.input(date=None, department='production')
+        return create_json(i.date, i.department)
 
 urls = (
-    '/', 'index'
+    '/', 'index',
+    '/report', 'TimeReport',
+    '/report/', 'TimeReport',
 )
+render = web.template.render('templates/')
 
 if __name__ == '__main__':
     app = web.application(urls, globals())
