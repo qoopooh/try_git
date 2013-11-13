2be5227cdcf9553baf24d64cf08f6c04eb869b50
phuchit aae <phuchit@aae.co.th>
2013-11-13 10:56:34 +0700

    generate from maven
    mvn archetype:generate \
    -DarchetypeGroupId=org.apache.maven.archetypes \
    -DgroupId=com.mycompany.app \
    -DartifactId=my-app

---
+++ java/gateway-ip/src/main/java/com/advanced-domotics/gatewayip/App.java
@@ -0,0 +1,13 @@
+package com.advanceddomotics.gatewayip;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
