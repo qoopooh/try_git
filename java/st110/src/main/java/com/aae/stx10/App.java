96d2bafb20f09dc26ae265ee845651e55a09ac45
phuchit aae <phuchit@aae.co.th>
2013-11-13 17:10:35 +0700

    list uart port on linux and windows

--- java/st110/src/main/java/com/aae/stx10/App.java
+++ java/st110/src/main/java/com/aae/stx10/App.java
@@ -1,13 +1,49 @@
 package com.aae.stx10;
 
+import java.util.Enumeration;
+
+import gnu.io.CommPort;
+import gnu.io.CommPortIdentifier;
+import gnu.io.SerialPort;
+
 /**
- * Hello world!
+ * ST110 USB
  *
  */
 public class App 
 {
+    static void listPorts()
+    {
+        java.util.Enumeration<CommPortIdentifier> portEnum = CommPortIdentifier.getPortIdentifiers();
+        while ( portEnum.hasMoreElements() ) 
+        {
+            CommPortIdentifier portIdentifier = portEnum.nextElement();
+            System.out.println(portIdentifier.getName()  +  " - " +  getPortTypeName(portIdentifier.getPortType()) );
+        }        
+    }
+
+    static String getPortTypeName ( int portType )
+    {
+        switch ( portType )
+        {
+            case CommPortIdentifier.PORT_I2C:
+                return "I2C";
+            case CommPortIdentifier.PORT_PARALLEL:
+                return "Parallel";
+            case CommPortIdentifier.PORT_RAW:
+                return "Raw";
+            case CommPortIdentifier.PORT_RS485:
+                return "RS485";
+            case CommPortIdentifier.PORT_SERIAL:
+                return "Serial";
+            default:
+                return "unknown type";
+        }
+    }
+
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        listPorts();
     }
 }
