4b8c6b6e6c30f0f2bfe79cd1a1a151601c27bed2
phuchit aae <phuchit@aae.co.th>
2013-11-13 13:51:08 +0700

    carlo want to test encryption

---
+++ codejam/carlo/encrypt.cpp
@@ -0,0 +1,94 @@
+//-----------------------------------------------------------------------------
+//   Advanced Domotics                        Copyright 2012
+//-----------------------------------------------------------------------------
+//
+//  Project:        FTC2E
+//
+//  Processor:      Texas Instruments MSP430F247
+//  Clock:          16 MHz
+//
+//-----------------------------------------------------------------------------
+//  Module:         encrypt.c
+//
+//  Contents:       encryption and decryption routines
+//
+//-----------------------------------------------------------------------------
+#include "encrypt.h"
+
+
+
+
+// GLOBAL VARIABLES
+
+
+//-------------------------------------
+//  Encrypt routine (draft)
+//    - only charaters and numbers are encrypted, symbols remain unchanged
+//    - result of encrypted char is always another char
+//    - routine uses a sequence of pw[] characters as password
+//-------------------------------------
+void Encrypt(unsigned char *msg, char phone_id)
+{
+  unsigned char i,shift;
+
+  encr_decr_message[0] = phone_id;
+  encr_decr_message[1] = ',';
+  i=2;              // first char is phone number [1,9], not encrypted
+  while (msg[i-2] != LINE_FEED)
+    {
+    shift = unit_id[i-1] + unit_id[0];
+    encr_decr_message[i] = msg[i-2];
+    if ((encr_decr_message[i] >= '0' && encr_decr_message[i] <= '9') || (encr_decr_message[i] >= 'A' && encr_decr_message[i] <= 'Z') || (encr_decr_message[i] >= 'a' && encr_decr_message[i] <= 'z'))
+      {
+      while (shift)
+        {
+        encr_decr_message[i]++;
+        if (encr_decr_message[i] > '9' && encr_decr_message[i] < 'A')
+          encr_decr_message[i] = 'A';
+        if (encr_decr_message[i] > 'Z' && encr_decr_message[i] < 'a')
+          encr_decr_message[i] = 'a';
+        if (encr_decr_message[i] > 'z')
+          encr_decr_message[i] = '0';
+        --shift;
+        }
+      }
+    i++;
+    }
+  encr_decr_message[i] = LINE_FEED;
+}
+
+
+//-------------------------------------
+//
+//-------------------------------------
+void Decrypt(unsigned char *encrypted_msg)
+{
+  unsigned char i,shift;
+  
+  i=0;              // first char is phone number [1,9], not encrypted
+  while (encrypted_msg[i+2] != LINE_FEED)
+    {
+    shift = unit_id[i+1] + unit_id[0];
+    encr_decr_message[i] = encrypted_msg[i+2];
+    if ((encr_decr_message[i] >= '0' && encr_decr_message[i] <= '9') || (encr_decr_message[i] >= 'A' && encr_decr_message[i] <= 'Z') || (encr_decr_message[i] >= 'a' && encr_decr_message[i] <= 'z'))
+      {
+      while (shift)
+        {
+        encr_decr_message[i]--;
+        if (encr_decr_message[i] < '0')
+          encr_decr_message[i] = 'z';
+        if (encr_decr_message[i] > '9' && encr_decr_message[i] < 'A')
+          encr_decr_message[i] = '9';
+        if (encr_decr_message[i] > 'Z' && encr_decr_message[i] < 'a')
+          encr_decr_message[i] = 'Z';
+        --shift;
+        }
+      }
+    i++;
+    }
+    encr_decr_message[i] = LINE_FEED;
+}
+
+
+
+
---
+++ codejam/carlo/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "encrypt.h"
+
+using namespace std;
+
+unsigned char encr_decr_message[MAX_TX_BUFF_LEN];      // encrypted message
+
+const char *unit_id = \
+"11111111112222222222333333333344444444441111111111222222222233333333334444444444";
+
+char msg[80];
+
+int main (int argc, char* argv[])
+{
+  cout << "Decrypt beginning" << endl;
+
+  return 0;
+}
