89810b664d1b18b816b97b531f5cd1447af2df8c
Berm Mini <mac@mini.berm>
2013-11-15 22:39:54 +0700

    http://zetcode.com/gui/tkinter/introduction/

--- python/tk/sample.py
+++ python/tk/sample.py
@@ -1,32 +1,32 @@
 #!/usr/bin/env python
 
-from Tkinter import *
+from Tkinter import Tk, BOTH, Frame, Button
 
 class App(Frame):
+
+    def __init__(self, parent):
+        Frame.__init__(self, parent, background='blue')
+        self.parent = parent
+        self.initUI()
+
     def say_hi(self):
         print "hi there, everyone!"
 
-    def createWidgets(self):
-        self.QUIT = Button(self)
-        self.QUIT["text"] = "QUIT"
-        self.QUIT["fg"] = "red"
-        self.QUIT["command"] = self.quit
-
-        self.QUIT.pack({"side": "left"})
+    def initUI(self):
+        self.QUIT = Button(self, text="Quit", fg="red", command=self.quit)
+        self.QUIT.place(x=50, y=5)
 
         self.hi_there = Button(self)
         self.hi_there["text"] = "Hello",
         self.hi_there["command"] = self.say_hi
-
         self.hi_there.pack({"side": "left"})
 
-    def __init__(self, master=None):
-        Frame.__init__(self, master)
-        self.pack()
-        self.createWidgets()
+        self.parent.title("Sample V.01")
+        self.pack(fill=BOTH, expand=1)
 
 root = Tk()
-app = App(master=root)
+root.geometry("250x100+50+25")
+app = App(root)
 app.mainloop()
 root.destroy()
 
