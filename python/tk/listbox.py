1704e0c57f32c8e804192df73b0d92421869c710
Berm Mini <mac@mini.berm>
2013-11-15 23:46:29 +0700

    listbox + scale

--- python/tk/listbox.py
+++ python/tk/listbox.py
@@ -12,8 +12,8 @@ last modified: December 2010
 website: www.zetcode.com
 """
 
-from ttk import Frame, Label, Style
-from Tkinter import Tk, BOTH, Listbox, StringVar, END
+from ttk import Frame, Label, Style, Scale
+from Tkinter import Tk, BOTH, Listbox, StringVar, END, IntVar
 
 
 class Example(Frame):
@@ -26,7 +26,7 @@ class Example(Frame):
 
     def initUI(self):
 
-        self.parent.title("Listbox")
+        self.parent.title("Listbox + Scale")
         self.pack(fill=BOTH, expand=1)
         acts = ['Scarlett Johansson', 'Rachel Weiss',
             'Natalie Portman', 'Jessica Alba']
@@ -40,16 +40,24 @@ class Example(Frame):
 
         self.var = StringVar()
         self.label = Label(self, text=0, textvariable=self.var)
-        self.label.place(x=20, y=210)
+        self.label.place(x=20, y=190)
+
+        scale = Scale(self, from_=0, to=100, command=self.onScale)
+        scale.place(x=20, y=220)
+
+        self.var_scale = IntVar()
+        self.label_scale = Label(self, text=0, textvariable=self.var_scale)
+        self.label_scale.place(x=150, y=220)
 
     def onSelect(self, val):
-
         sender = val.widget
         idx = sender.curselection()
         value = sender.get(idx)
-
         self.var.set(value)
 
+    def onScale(self, val):
+        v = int(float(val))
+        self.var_scale.set(v)
 
 def main():
 
