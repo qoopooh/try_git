a7312939dd18ef6fc255b2e058a1ee457c30da0b
Berm Mini <mac@mini.berm>
2013-11-15 23:56:07 +0700

    + checkbox

--- python/tk/listbox.py
+++ python/tk/listbox.py
@@ -13,7 +13,7 @@ website: www.zetcode.com
 """
 
 from ttk import Frame, Label, Style, Scale
-from Tkinter import Tk, BOTH, Listbox, StringVar, END, IntVar
+from Tkinter import Tk, BOTH, Listbox, StringVar, END, IntVar, Checkbutton
 
 
 class Example(Frame):
@@ -26,7 +26,7 @@ class Example(Frame):
 
     def initUI(self):
 
-        self.parent.title("Listbox + Scale")
+        self.parent.title("Listbox + Scale + ChkBtn")
         self.pack(fill=BOTH, expand=1)
         acts = ['Scarlett Johansson', 'Rachel Weiss',
             'Natalie Portman', 'Jessica Alba']
@@ -47,7 +47,13 @@ class Example(Frame):
 
         self.var_scale = IntVar()
         self.label_scale = Label(self, text=0, textvariable=self.var_scale)
-        self.label_scale.place(x=150, y=220)
+        self.label_scale.place(x=180, y=220)
+
+        self.var_chk = IntVar()
+        cb = Checkbutton(self, text="Test", variable=self.var_chk,
+                command=self.onClick)
+        cb.select()
+        cb.place(x=220, y=60)
 
     def onSelect(self, val):
         sender = val.widget
@@ -58,6 +64,12 @@ class Example(Frame):
     def onScale(self, val):
         v = int(float(val))
         self.var_scale.set(v)
+
+    def onClick(self):
+        if self.var_chk.get() == 1:
+            self.var.set("checked")
+        else:
+            self.var.set("unchecked")
 
 def main():
 
