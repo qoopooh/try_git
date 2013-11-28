14664065e9368ef1d7dcaf40fba35eed7c534556
phuchit aae <phuchit@aae.co.th>
2013-11-28 11:09:28 +0700

    filter host

--- java/usr/src/com/atouch/Usr.java
+++ java/usr/src/com/atouch/Usr.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.SocketTimeoutException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Usr {
@@ -15,24 +17,37 @@ public class Usr {
 
     static final int UDP_PORT = 1500;
 
-    public static List<Usr> getList() throws IOException {
-        InetAddress IPAddress = InetAddress.getByName("localhost");
+    public Usr(byte[] data) {
+        // TODO Auto-generated constructor stub
+    }
 
+    public static List<Usr> getList() throws IOException {
+        String hostname = InetAddress.getLocalHost().getHostName();
+        InetAddress host = InetAddress.getByName(hostname);
         DatagramSocket serverSocket = new DatagramSocket(UDP_PORT);
-        // DatagramPacket receivePacket = null;
-        // serverSocket.receive(receivePacket);
         byte[] buf = BROADCAST_MSG.getBytes();
         InetAddress address = InetAddress.getByName(ALL_IP);
         DatagramPacket packet = new DatagramPacket(buf, buf.length,
                 address, UDP_PORT);
         serverSocket.send(packet);
+        serverSocket.setSoTimeout(500); // set the timeout in millisecounds.
 
         byte[] rcv = new byte[32];
         packet = new DatagramPacket(rcv, rcv.length);
-        serverSocket.receive(packet);
-        serverSocket.receive(packet);
+        ArrayList<Usr> out = new ArrayList<Usr>();
+        while (true) {
+            try {
+                serverSocket.receive(packet);
+                if (packet.getAddress().getHostName().equals(hostname))
+                    continue;
+                Usr usr = new Usr(packet.getData());
+                out.add(usr);
+            } catch (SocketTimeoutException e) {
+                break;
+            }
+        }
 
         serverSocket.close();
-        return null;
+        return out;
     }
 }
