6a4d2a6f180c46c82f3b4413306df03e625de206
phuchit aae <phuchit@aae.co.th>
2013-11-28 08:40:09 +0700

    http://docs.oracle.com/javase/tutorial/displayCode.html?code=http://docs.oracle.com/javase/tutorial/uiswing/examples/learn/CelsiusConverterProject/src/learn/CelsiusConverterGUI.java

--- java/usr/src/com/atouch/Gui.java
+++ java/usr/src/com/atouch/Gui.java
@@ -1,37 +1,124 @@
+
 package com.atouch;
 
-import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
 
 import javax.swing.JButton;
-import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.SwingUtilities;
+import javax.swing.JTextField;
 
-public class Gui implements Runnable  {
+public class Gui extends javax.swing.JFrame {
 
-	/**
-	 * @param args
+    /**
+	 * 
 	 */
-	public static void main(String[] args) {
-		Gui gui = new Gui();
-		SwingUtilities.invokeLater(gui);
-	}
+    private static final long serialVersionUID = 1L;
+    private JTextField tempTextField;
+    private JLabel celsiusLabel;
+    private JButton convertButton;
+    private JLabel fahrenheitLabel;
 
-	@Override
-	public void run() {
-        // Create the window
-        JFrame f = new JFrame("Hello, World!");
-        // Sets the behavior for when the window is closed
-        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        // Add a layout manager so that the button is not placed on top of the label
-        f.setLayout(new FlowLayout());
-        // Add a label and a button
-        f.add(new JLabel("Hello, world!"));
-        f.add(new JButton("Press me!"));
-        // Arrange the components inside the window
-        f.pack();
-        // By default, the window is not visible. Make it visible.
-        f.setVisible(true);
-	}
+    public Gui() {
+        initComponents();
+    }
+
+    private void initComponents() {
+        tempTextField = new javax.swing.JTextField();
+        celsiusLabel = new javax.swing.JLabel();
+        convertButton = new javax.swing.JButton();
+        fahrenheitLabel = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Celsius Converter");
+
+        celsiusLabel.setText("Celsius");
+
+        convertButton.setText("Convert");
+        convertButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                convertButtonActionPerformed(evt);
+            }
+        });
+
+        fahrenheitLabel.setText("Fahrenheit");
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(
+                getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(layout
+                .createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(
+                        layout.createSequentialGroup()
+                                .addContainerGap()
+                                .addGroup(
+                                        layout.createParallelGroup(
+                                                javax.swing.GroupLayout.Alignment.LEADING)
+                                                .addGroup(
+                                                        layout.createSequentialGroup()
+                                                                .addComponent(
+                                                                        tempTextField,
+                                                                        javax.swing.GroupLayout.PREFERRED_SIZE,
+                                                                        javax.swing.GroupLayout.DEFAULT_SIZE,
+                                                                        javax.swing.GroupLayout.PREFERRED_SIZE)
+                                                                .addPreferredGap(
+                                                                        javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                                                .addComponent(
+                                                                        celsiusLabel))
+                                                .addGroup(
+                                                        layout.createSequentialGroup()
+                                                                .addComponent(
+                                                                        convertButton)
+                                                                .addPreferredGap(
+                                                                        javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                                                .addComponent(
+                                                                        fahrenheitLabel)))
+                                .addContainerGap(27, Short.MAX_VALUE)));
+
+        layout.linkSize(javax.swing.SwingConstants.HORIZONTAL,
+                new java.awt.Component[] {
+                        convertButton, tempTextField
+                });
+
+        layout.setVerticalGroup(layout
+                .createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(
+                        layout.createSequentialGroup()
+                                .addContainerGap()
+                                .addGroup(
+                                        layout.createParallelGroup(
+                                                javax.swing.GroupLayout.Alignment.BASELINE)
+                                                .addComponent(
+                                                        tempTextField,
+                                                        javax.swing.GroupLayout.PREFERRED_SIZE,
+                                                        javax.swing.GroupLayout.DEFAULT_SIZE,
+                                                        javax.swing.GroupLayout.PREFERRED_SIZE)
+                                                .addComponent(celsiusLabel))
+                                .addPreferredGap(
+                                        javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addGroup(
+                                        layout.createParallelGroup(
+                                                javax.swing.GroupLayout.Alignment.BASELINE)
+                                                .addComponent(convertButton)
+                                                .addComponent(fahrenheitLabel))
+                                .addContainerGap(21, Short.MAX_VALUE)));
+        pack();
+    }
+
+    protected void convertButtonActionPerformed(ActionEvent evt) {
+        int tempFahr = (int) ((Double.parseDouble(tempTextField.getText()))
+                * 1.8 + 32);
+        fahrenheitLabel.setText(tempFahr + " Fahrenheit");
+    }
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Gui().setVisible(true);
+            }
+        });
+    }
 
 }
