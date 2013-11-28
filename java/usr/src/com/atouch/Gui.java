d692287f79a17374225cdf811a9ceb3ac142536c
phuchit aae <phuchit@aae.co.th>
2013-11-28 08:15:07 +0700

    press me button

--- java/usr/src/com/atouch/Gui.java
+++ java/usr/src/com/atouch/Gui.java
@@ -1,13 +1,37 @@
 package com.atouch;
 
-public class Gui {
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingUtilities;
+
+public class Gui implements Runnable  {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Gui gui = new Gui();
+		SwingUtilities.invokeLater(gui);
+	}
 
+	@Override
+	public void run() {
+        // Create the window
+        JFrame f = new JFrame("Hello, World!");
+        // Sets the behavior for when the window is closed
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // Add a layout manager so that the button is not placed on top of the label
+        f.setLayout(new FlowLayout());
+        // Add a label and a button
+        f.add(new JLabel("Hello, world!"));
+        f.add(new JButton("Press me!"));
+        // Arrange the components inside the window
+        f.pack();
+        // By default, the window is not visible. Make it visible.
+        f.setVisible(true);
 	}
 
 }
