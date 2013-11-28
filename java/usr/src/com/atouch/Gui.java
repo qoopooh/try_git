dc7d86e6856db8e9d6ae1819d3468edb64da4932
phuchit aae <phuchit@aae.co.th>
2013-11-28 17:33:08 +0700

    Domotics Gateway Setting V.1.0

--- java/usr/src/com/atouch/Gui.java
+++ java/usr/src/com/atouch/Gui.java
@@ -22,6 +22,7 @@ public class Gui extends javax.swing.JFrame {
     /**
 	 * 
 	 */
+    public static final String VERSION = "1.0";
     private static final long serialVersionUID = 1L;
     private JButton updateButton;
     private JLabel ipLabel;
@@ -45,7 +46,7 @@ public class Gui extends javax.swing.JFrame {
         portTextField = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setTitle("Domotics Gateway Setting");
+        setTitle("Domotics Gateway Setting V." + VERSION);
 
         updateButton.setText("Update");
         updateButton.addActionListener(new java.awt.event.ActionListener() {
