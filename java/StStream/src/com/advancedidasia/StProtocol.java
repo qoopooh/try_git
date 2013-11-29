aa6a86275650a3f3930ce61b23e71e9d8b3fb1ba
phuchit aae <phuchit@aae.co.th>
2013-11-29 15:26:23 +0700

    break

---
+++ java/StStream/src/com/advancedidasia/StProtocol.java
@@ -0,0 +1,13 @@
+
+package com.advancedidasia;
+
+public class StProtocol {
+
+    public enum Command {
+        GetSerialNumber,
+        Udp,
+        UdpServer,
+        TcpServer,
+        Unknown
+    }
+}
