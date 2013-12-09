32478f1da4818ccdf91122411fe9cd00d12de67c
phuchit aae <phuchit@aae.co.th>
2013-12-09 11:11:17 +0700

    remove unused files in domotics

---
+++ python/utils/remove-domotics-files.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import os
+
+for root, dirs, files in os.walk("./"):
+    for name in files:
+        if name.endswith((".bak",)):
+            os.remove(os.path.join(root, name))
+            print name
+        if "conflicted" in name:
+            os.remove(os.path.join(root, name))
+            print name
+
+
