ebec21330dc9ac6bbbcd7b884a0250a322a46130
phuchit aae <phuchit@aae.co.th>
2013-12-09 11:15:50 +0700

    object files

--- python/utils/remove-domotics-files.py
+++ python/utils/remove-domotics-files.py
@@ -4,7 +4,7 @@ import os
 
 for root, dirs, files in os.walk("./"):
     for name in files:
-        if name.endswith((".bak",)):
+        if name.endswith((".bak", ".o", ".hzo", ".hzx", ".eep")):
             os.remove(os.path.join(root, name))
             print name
         if "conflicted" in name:
