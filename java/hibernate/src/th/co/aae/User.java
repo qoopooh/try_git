a56b2b0cc8d7a2c2d5ac7d452200697d9f33c5af
phuchit aae <phuchit@aae.co.th>
2013-12-18 16:16:47 +0700

    Java hibernate on MSSQL (aaebio)
    can run only 1 record

---
+++ java/hibernate/src/th/co/aae/User.java
@@ -0,0 +1,40 @@
+
+package th.co.aae;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity(name = "User_table")
+public class User {
+    @Id
+    int userId;
+    @Column(name = "User_Name")
+    String userName;
+
+    String userMessage;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserMessage() {
+        return userMessage;
+    }
+
+    public void setUserMessage(String userMessage) {
+        this.userMessage = userMessage;
+    }
+}
