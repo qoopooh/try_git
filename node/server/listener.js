fb17c6e794520bdeb61662b94740bf98d4d4dfb7
berm wheezy <berm@stable.deb>
2013-12-25 16:38:15 +0700

    server test

---
+++ node/server/listener.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env nodejs
+
+var PORT = 8001;
+
+var count = 0;
+var http = require('http');
+var server = http.createServer(function(req, resp) {
+  console.log(req.method, ++count, req.headers['user-agent']);
+
+  resp.writeHead(200, {'Content-Type':'text/plain'});
+  for (var i=0; i<100; i++) {
+    resp.write('text:item: ' + i);
+  }
+
+  var data = [];
+  for (var i=0; i<100; i++) {
+    if (i % 2 == 0) {
+      data.push('other:item. ' + i);
+    } else {
+      for (var x=0; x<i; x++) {
+        data.push('other:subitem. ' + x);
+      }
+    }
+  }
+
+  resp.end(data.join('\n'));
+});
+
+server.listen(PORT);
+console.log('Server on:', PORT);
+
