9c9a44fcc94a8067d41950e91aab1aea72df55bb
berm wheezy <berm@stable.deb>
2013-12-25 17:13:14 +0700

    ArrayBuffer converter

---
+++ node/abuf.js
@@ -0,0 +1,19 @@
+function toArrayBuffer(buffer) {
+  var ab = new ArrayBuffer(buffer.length);
+  var view = new Uint8Array(ab);
+  for (var i = 0; i < buffer.length; ++i) {
+    view[i] = buffer[i];
+  }
+  return ab;
+}
+
+function toBuffer(ab) {
+  var buffer = new Buffer(ab.byteLength);
+  var view = new Uint8Array(ab);
+  for (var i = 0; i < buffer.length; ++i) {
+    buffer[i] = view[i];
+  }
+  return buffer;
+}
+
+
