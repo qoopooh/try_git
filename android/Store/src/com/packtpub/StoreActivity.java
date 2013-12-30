76285b80b3754566c644ea8fc2483c7a123b8450
pooh arch64 <pooh@arch64.n570>
2013-12-30 23:42:56 +0700

    android create project -p ./Store -n Store -t android-19 -k com.packtpub -a StoreActivity create project from book chapter 2

---
+++ android/Store/src/com/packtpub/StoreActivity.java
@@ -0,0 +1,15 @@
+package com.packtpub;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class StoreActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
