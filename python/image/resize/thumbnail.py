3bd5992cfdb8692724e490f7233593deace283d0
ads mac <qoopooh@mac.ads>
2014-02-01 08:06:56 +0700

    run on ads

--- python/image/resize/thumbnail.py
+++ python/image/resize/thumbnail.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import os, sys
 import Image
 
@@ -7,10 +9,10 @@ for infile in sys.argv[1:]:
     outfile = os.path.join(os.path.dirname(os.path.realpath(__file__)), os.path.basename(infile))
     print outfile
     if infile != outfile:
-        try:
-            im = Image.open(infile)
-            im.thumbnail(size, Image.ANTIALIAS)
-            im.save(outfile, "JPEG")
-        except IOError:
-            print "cannot create thumbnail for '%s'" % infile
+#try:
+        im = Image.open(infile)
+        im.thumbnail(size, Image.ANTIALIAS)
+        im.save(outfile, "JPEG")
+#except IOError:
+#print "cannot create thumbnail for '%s'" % infile
 
