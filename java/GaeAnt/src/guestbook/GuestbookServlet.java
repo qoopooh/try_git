d0669651f9c7e7fe508516d6a94f71fb576e4ead
ads mac <qoopooh@mac.ads>
2014-02-04 13:31:34 +0700

    use jsp

--- java/GaeAnt/src/guestbook/GuestbookServlet.java
+++ java/GaeAnt/src/guestbook/GuestbookServlet.java
@@ -2,13 +2,23 @@ package guestbook;
 
 import java.io.IOException;
 import javax.servlet.http.*;
+import com.google.appengine.api.users.User;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
 
 public class GuestbookServlet extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws IOException {
-        resp.setContentType("text/plain");
-        resp.getWriter().println("Hello, 2");
+        UserService userService = UserServiceFactory.getUserService();
+        User user = userService.getCurrentUser();
+
+        if (user != null) {
+          resp.setContentType("text/plain");
+          resp.getWriter().println("Hello, " + user.getNickname());
+        } else {
+          resp.sendRedirect(userService.createLoginURL(req.getRequestURI()));
+        }
     }
 }
 
