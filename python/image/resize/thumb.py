837e586b141d49903c6eda76f7b4ee6ca16eb984
ads mac <qoopooh@mac.ads>
2014-02-04 16:33:47 +0700

    resize multiple extensions

--- python/image/resize/thumb.py
+++ python/image/resize/thumb.py
@@ -5,18 +5,19 @@ import Image
 from glob import glob
 
 THUMB = "-thumb"
-
-size = 1024, 768
+SIZE = 1024, 768
+TYPES = ('*.jpg', '*.JPG', '*.png', '*.jpeg')
 
 def resize(infile):
     if THUMB in infile:
         return
     outfile = os.path.splitext(infile)
     im = Image.open(infile)
-    im.thumbnail(size, Image.ANTIALIAS)
+    im.thumbnail(SIZE, Image.ANTIALIAS)
     im.save(outfile[0] + THUMB + outfile[1], "JPEG")
     print 'Thumbnailing', infile
     
-for fn in glob('*.jpg'):
-    resize(fn)
+for files in TYPES:
+    for fn in glob(files):
+        resize(fn)
 
