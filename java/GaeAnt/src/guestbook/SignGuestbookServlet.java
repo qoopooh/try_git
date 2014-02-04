b1f5bcad24d1223862a8e9432d058567b1327fe4
ads mac <qoopooh@mac.ads>
2014-02-04 14:44:26 +0700

    prepare for https://developers.google.com/appengine/docs/java/gettingstarted/usingdatastore

--- java/GaeAnt/src/guestbook/SignGuestbookServlet.java
+++ java/GaeAnt/src/guestbook/SignGuestbookServlet.java
@@ -1,11 +1,21 @@
 package guestbook;
 
-import java.io.IOException;
-import java.util.logging.Logger;
-import javax.servlet.http.*;
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
 import com.google.appengine.api.users.User;
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
+
+import java.io.IOException;
+import java.util.Date;
+import java.util.logging.Logger;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public class SignGuestbookServlet extends HttpServlet {
     private static final Logger log = Logger.getLogger(SignGuestbookServlet.class.getName());
