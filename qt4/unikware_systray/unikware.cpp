4e49cc67b4f14b5388d6372be29da1e58f010c42
phuchit aae <phuchit@aae.co.th>
2014-02-13 17:56:46 +0700

    crash on close

--- qt4/unikware_systray/unikware.cpp
+++ qt4/unikware_systray/unikware.cpp
@@ -4,13 +4,13 @@
 Unikware::Unikware(QWidget *parent)
     : QDialog(parent)
 {
-    createIconGroupBox();
-    createMessageGroupBox();
+  createIconGroupBox();
+  createMessageGroupBox();
 
 //    iconLabel->setMinimumWidth(durationLabel->sizeHint().width());
 
-    createActions();
-    createTrayIcon();
+  createActions();
+  createTrayIcon();
 }
 
 Unikware::~Unikware()
@@ -18,39 +18,56 @@ Unikware::~Unikware()
 
 }
 
+void Unikware::createIconGroupBox()
+{
+  iconGroupBox = new QGroupBox(tr("Tray Icon"));
+
+  iconLabel = new QLabel("Icon:");
+
+}
+
+void Unikware::createMessageGroupBox()
+{
+
+}
 
 void Unikware::createActions()
 {
-    minimizeAction = new QAction(tr("Mi&nimize"), this);
-    connect(minimizeAction, SIGNAL(triggered()), this, SLOT(hide()));
+  minimizeAction = new QAction(tr("Mi&nimize"), this);
+  connect(minimizeAction, SIGNAL(triggered()), this, SLOT(hide()));
 
-    maximizeAction = new QAction(tr("Ma&ximize"), this);
-    connect(maximizeAction, SIGNAL(triggered()), this, SLOT(showMaximized()));
+  maximizeAction = new QAction(tr("Ma&ximize"), this);
+  connect(maximizeAction, SIGNAL(triggered()), this, SLOT(showMaximized()));
 
-    restoreAction = new QAction(tr("&Restore"), this);
-    connect(restoreAction, SIGNAL(triggered()), this, SLOT(showNormal()));
+  restoreAction = new QAction(tr("&Restore"), this);
+  connect(restoreAction, SIGNAL(triggered()), this, SLOT(showNormal()));
 
-    quitAction = new QAction(tr("&Quit"), this);
-    connect(quitAction, SIGNAL(triggered()), qApp, SLOT(quit()));
+  quitAction = new QAction(tr("&Quit"), this);
+  connect(quitAction, SIGNAL(triggered()), qApp, SLOT(quit()));
+}
+
+void Unikware::createTrayIcon()
+{
+
 }
 
 void Unikware::setVisible(bool visible)
 {
-    minimizeAction->setEnabled(visible);
-    maximizeAction->setEnabled(!isMaximized());
-    restoreAction->setEnabled(isMaximized() || !visible);
-    QDialog::setVisible(visible);
+  minimizeAction->setEnabled(visible);
+  maximizeAction->setEnabled(!isMaximized());
+  restoreAction->setEnabled(isMaximized() || !visible);
+  QDialog::setVisible(visible);
 }
 
 void Unikware::closeEvent(QCloseEvent *event)
 {
-    if (trayIcon->isVisible()) {
-        QMessageBox::information(this, tr("Systray"),
-                                 tr("The program will keep running in the "
-                                    "system tray. To terminate the program, "
-                                    "choose <b>Quit</b> in the context menu "
-                                    "of the system tray entry."));
-        hide();
-        event->ignore();
-    }
+  if (trayIcon->isVisible()) {
+    QMessageBox::information(this, tr("Systray"),
+        tr("The program will keep running in the "
+        "system tray. To terminate the program, "
+        "choose <b>Quit</b> in the context menu "
+        "of the system tray entry."));
+    hide();
+    event->ignore();
+  }
 }
