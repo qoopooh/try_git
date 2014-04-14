6ba40b4a7d085020eec2250452d2acdf67458dae
Pooh CentOS32 <pooh@acer.centos>
2014-04-14 11:16:16 +0700

    backup exists

---
+++ sqlite/eurosoft/rej-casing.sql
@@ -0,0 +1,60 @@
+
+--SELECT RejectTrans_Create_Date, RejectTrans_ID
+--FROM tblRejectTransaction
+--ORDER BY RejectTrans_Create_Date
+
+--SELECT * FROM tblRejectStock
+
+SELECT mx.*, CASE WHEN (SELECT COUNT(*) FROM tblRejectStock WHERE RejectStock_Casing_ID=mx.casing)  > 0 THEN 1 ELSE 0 END InStock
+FROM tblCasing,
+(SELECT RejectTransDetail_RejectTrans_ID tid, MAX(RejectTransDetail_Serial) sn, RejectTransDetail_Casing_ID casing
+FROM tblRejectTransactionDetail
+WHERE RejectTransDetail_RejectTrans_ID LIKE 'RJI130900%'
+GROUP BY RejectTransDetail_RejectTrans_ID
+ORDER BY tid) mx
+WHERE mx.casing=Casing_ID
+
+--SELECT mx.*, CASE WHEN (SELECT COUNT(*) FROM tblRejectStock WHERE RejectStock_Casing_ID=mx.casing)  > 0 THEN 1 ELSE 0 END cond1
+--FROM tblCasing,
+--(SELECT RejectTransDetail_RejectTrans_ID tid, MAX(RejectTransDetail_Serial) sn, RejectTransDetail_Casing_ID casing
+--FROM tblRejectTransactionDetail
+--WHERE RejectTransDetail_RejectTrans_ID LIKE 'RJI%'
+  --AND RejectTransDetail_Serial=68
+--GROUP BY RejectTransDetail_RejectTrans_ID
+--ORDER BY tid) mx
+--WHERE mx.casing=Casing_ID
+
+--SELECT mx.*, CASE WHEN Casing_Owner_ID>50 THEN Casing_Owner_ID ELSE Casing_Tyre_Serial END cond1
+--FROM tblCasing,
+--(SELECT RejectTransDetail_RejectTrans_ID tid, MAX(RejectTransDetail_Serial) sn, RejectTransDetail_Casing_ID casing
+--FROM tblRejectTransactionDetail
+--WHERE RejectTransDetail_RejectTrans_ID LIKE 'RJI%'
+  --AND RejectTransDetail_Serial=68
+--GROUP BY RejectTransDetail_RejectTrans_ID
+--ORDER BY tid) mx
+--WHERE mx.casing=Casing_ID
+
+--SELECT mx.*, Comp_Name name
+--FROM tblCompany, tblCasing,
+--(SELECT RejectTransDetail_RejectTrans_ID tid, MAX(RejectTransDetail_Serial) sn, RejectTransDetail_Casing_ID casing
+--FROM tblRejectTransactionDetail
+--WHERE RejectTransDetail_RejectTrans_ID LIKE 'RJI%'
+  --AND RejectTransDetail_Serial=68
+--GROUP BY RejectTransDetail_RejectTrans_ID
+--ORDER BY tid) mx
+--WHERE mx.casing=Casing_ID
+  --AND Casing_Owner_ID=Comp_ID
+
+--SELECT RejectTransDetail_RejectTrans_ID id, RejectTransDetail_Serial
+--FROM tblRejectTransactionDetail
+--WHERE RejectTransDetail_RejectTrans_ID LIKE 'RJI131%'
+
+--SELECT RejectTransDetail_RejectTrans_ID tid, MAX(RejectTransDetail_Serial) sn, RejectTransDetail_Casing_ID casing
+--FROM tblRejectTransactionDetail
+--WHERE RejectTransDetail_RejectTrans_ID LIKE 'RJI%'
+  --AND RejectTransDetail_Serial > 50
+--GROUP BY RejectTransDetail_RejectTrans_ID
+--ORDER BY tid
+
+;
+
