fe01454f9300e5294baa40ee2304c904c1820867
Pooh CentOS32 <pooh@acer.centos>
2014-04-14 11:45:14 +0700

    outer join

--- sqlite/eurosoft/rej-casing.sql
+++ sqlite/eurosoft/rej-casing.sql
@@ -1,18 +1,37 @@
 
+SELECT mx.tid, RejectTrans_Create_Date date,
+  CASE WHEN
+    (SELECT COUNT(*) FROM tblRejectStock WHERE RejectStock_Casing_ID=mx.casing)>0
+  THEN 1 ELSE 0
+  END InStock, Comp_Name
+FROM 
+(SELECT RejectTransDetail_RejectTrans_ID tid, MAX(RejectTransDetail_Serial) sn, RejectTransDetail_Casing_ID casing
+FROM tblRejectTransactionDetail
+WHERE RejectTransDetail_RejectTrans_ID LIKE 'RJO13%'
+GROUP BY RejectTransDetail_RejectTrans_ID
+ORDER BY tid) mx
+LEFT OUTER JOIN tblCasing
+ON mx.casing=Casing_ID
+LEFT OUTER JOIN tblCompany
+ON Casing_Owner_ID=Comp_ID
+LEFT OUTER JOIN tblRejectTransaction
+ON mx.tid=RejectTrans_ID
+
+--SELECT mx.tid, Comp_Name, CASE WHEN (SELECT COUNT(*) FROM tblRejectStock WHERE RejectStock_Casing_ID=mx.casing)  > 0 THEN 1 ELSE 0 END InStock
+--FROM tblCasing, tblCompany, 
+--(SELECT RejectTransDetail_RejectTrans_ID tid, MAX(RejectTransDetail_Serial) sn, RejectTransDetail_Casing_ID casing
+--FROM tblRejectTransactionDetail
+--WHERE RejectTransDetail_RejectTrans_ID LIKE 'RJI130900%'
+--GROUP BY RejectTransDetail_RejectTrans_ID
+--ORDER BY tid) mx
+--WHERE mx.casing=Casing_ID
+  --AND Casing_Owner_ID=Comp_ID
+
 --SELECT RejectTrans_Create_Date, RejectTrans_ID
 --FROM tblRejectTransaction
 --ORDER BY RejectTrans_Create_Date
 
 --SELECT * FROM tblRejectStock
-
-SELECT mx.*, CASE WHEN (SELECT COUNT(*) FROM tblRejectStock WHERE RejectStock_Casing_ID=mx.casing)  > 0 THEN 1 ELSE 0 END InStock
-FROM tblCasing,
-(SELECT RejectTransDetail_RejectTrans_ID tid, MAX(RejectTransDetail_Serial) sn, RejectTransDetail_Casing_ID casing
-FROM tblRejectTransactionDetail
-WHERE RejectTransDetail_RejectTrans_ID LIKE 'RJI130900%'
-GROUP BY RejectTransDetail_RejectTrans_ID
-ORDER BY tid) mx
-WHERE mx.casing=Casing_ID
 
 --SELECT mx.*, CASE WHEN (SELECT COUNT(*) FROM tblRejectStock WHERE RejectStock_Casing_ID=mx.casing)  > 0 THEN 1 ELSE 0 END cond1
 --FROM tblCasing,
