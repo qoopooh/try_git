2c7ce8e7a29b8f38352e2d8be27413c62bbabf0a
Berm Mini <mac@mini.berm>
2014-05-31 22:26:43 +0700

    Use external interrupt and timer interrupt to remove debouncing

--- arm/stm32/shutt/main.c
+++ arm/stm32/shutt/main.c
@@ -5,8 +5,39 @@
 
 const int TIMER_PSC_1SEC = 10000;
 const int TIMER_ARR_1SEC = 4800;
+const int TIMER_ARR_DEBNC = 480;
 
 uint32_t state;
+
+void TIM2_IRQHandler(void)
+{
+  static uint16_t counter=0;
+
+  if ((++counter & 0x0001) == 0)
+    SET_LED2_OFF;
+  else
+    SET_LED2_ON;
+  TIM2->SR &= ~(TIM_SR_UIF);
+}
+
+void TIM3_IRQHandler(void)
+{
+  TIM3->CR1 &= ~TIM_CR1_CEN;    // Timer 3 up-counter is disable
+  TIM3->SR &= ~(TIM_SR_UIF);
+
+  EXTI->IMR |= EXTI_IMR_MR1;
+}
+
+void EXTI1_IRQHandler(void)
+{
+  if (READ_BIT(GPIOB->IDR, BIT9))
+    SET_LED1_ON;
+  else
+    SET_LED1_OFF;
+  EXTI->IMR &= ~EXTI_IMR_MR1;
+  EXTI->PR |= EXTI_PR_PR1;
+  TIM3->CR1 |= TIM_CR1_CEN;     // Timer 3 up-counter is enable
+}
 
 void delay(int count)
 {
@@ -20,37 +51,48 @@ void board_init(void)
   uint32_t mode;
   uint32_t pin;
 
-  RCC->CFGR |= RCC_CFGR_PLLSRC; // HSE oscillator clock selected as PLL input clock
-  RCC->CFGR &= ~(RCC_CFGR_PLLXTPRE);   // no HSE divider for PLL entry
-  RCC->CFGR |= RCC_CFGR_PLLMULL_2; // 0100: PLL input clock x6 = 8MHz * 8 = 48MHz
+  RCC->CFGR |= RCC_CFGR_PLLSRC;       // HSE clock selected as PLL input clock
+  RCC->CFGR &= ~(RCC_CFGR_PLLXTPRE);  // no HSE divider for PLL entry
+  RCC->CFGR |= RCC_CFGR_PLLMULL_2;    // PLL input clock x6 = 8MHz * 8 = 48MHz
   RCC->CFGR |= RCC_CFGR_SW_PLL;
-  RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
   RCC->CR |= RCC_CR_HSEON | RCC_CR_PLLON;
   while ((RCC->CR & RCC_CR_PLLRDY) == 0);
-  SET_BIT(RCC->APB2ENR, RCC_APB2ENR_IOPBEN);
+
+  // Enable clock of timer
+  RCC->APB1ENR |= RCC_APB1ENR_TIM3EN | RCC_APB1ENR_TIM2EN;
+
+  // Enable clock of GPIO
   SET_BIT(RCC->APB2ENR, RCC_APB2ENR_IOPCEN);
+  SET_BIT(RCC->APB2ENR, RCC_APB2ENR_IOPBEN);
+  SET_BIT(RCC->APB2ENR, RCC_APB2ENR_AFIOEN);
 
   // Set OUTPUT
   pin = 1;
   mode = (uint32_t) 0x05 << (pin * 4);
   mask = (uint32_t) 0x0f << (pin * 4);
-  MODIFY_REG(GPIOB->CRH, mask, mode); // BIT9 output 10 MHz open-drain
+  MODIFY_REG(GPIOB->CRH, mask, mode);   // BIT9 output 10 MHz open-drain
+
   pin = 0;
   mode = (uint32_t) 0x05 << (pin * 4);
   mask = (uint32_t) 0x0f << (pin * 4);
-  MODIFY_REG(GPIOB->CRH, mask, mode); // BIT8 output 10 MHz open-drain
+  MODIFY_REG(GPIOB->CRH, mask, mode);   // BIT8 output 10 MHz open-drain
 
   // Set INPUT
   pin = 0;
   mode = (uint32_t) 0x08 << (pin * 4);
   mask = (uint32_t) 0x0f << (pin * 4);
-  MODIFY_REG(GPIOC->CRL, mask, mode); // BIT0 input push-pull
-  SET_BIT(GPIOC->ODR, BIT0); // Set PC0 to high for input
+  MODIFY_REG(GPIOC->CRL, mask, mode);   // BIT0 input push-pull
+  SET_BIT(GPIOC->ODR, BIT0);            // Set PC0 to high for input
+
   pin = 1;
   mode = (uint32_t) 0x08 << (pin * 4);
   mask = (uint32_t) 0x0f << (pin * 4);
-  MODIFY_REG(GPIOC->CRL, mask, mode); // BIT1 input push-pull
-  SET_BIT(GPIOC->ODR, BIT1); // Set PC0 to high for input
+  MODIFY_REG(GPIOC->CRL, mask, mode);   // BIT1 input push-pull
+  SET_BIT(GPIOC->ODR, BIT1);            // Set PC0 to high for input
+  AFIO->EXTICR[0] |= AFIO_EXTICR1_EXTI1_PC;
+  EXTI->IMR |= EXTI_IMR_MR1;
+  EXTI->EMR |= EXTI_EMR_MR1;
+  EXTI->FTSR |= EXTI_FTSR_TR1;
 
   SET_LED1_OFF;
   SET_LED2_OFF;
@@ -58,12 +100,24 @@ void board_init(void)
 
 void timer_init(void)
 {
-  TIM2->PSC = TIMER_PSC_1SEC; // prescaler
-  TIM2->ARR = TIMER_ARR_1SEC; // autoreload
+  TIM2->PSC = TIMER_PSC_1SEC;       // prescaler
+  TIM2->ARR = TIMER_ARR_1SEC;       // autoreload
   TIM2->DIER |= TIM_DIER_UIE;
+  TIM2->CR1 |= TIM_CR1_CEN;         // Timer 2 up-counter is enable
 
-  TIM2->CR1 |= TIM_CR1_CEN; // Timer 2 upcounter is enable
+  TIM3->PSC = TIMER_PSC_1SEC;       // prescaler
+  TIM3->ARR = TIMER_ARR_DEBNC;      // autoreload
+  TIM3->DIER |= TIM_DIER_UIE;
 }
+
+//################## Interrupt Configuration Routine #########################
+void NVIC_Configuration(void)
+{
+  NVIC_EN0_R |= (1<<TIM2_IRQn);     // Enable Timer 2
+  NVIC_EN0_R |= (1<<TIM3_IRQn);     // Enable Timer 3
+  NVIC_EN0_R |= (1<<EXTI1_IRQn);    // Enable EXTI 1
+}
+
 
 void set_leds(unsigned on)
 {
@@ -76,45 +130,16 @@ void set_leds(unsigned on)
 int get_button(void)
 {
   if (BUTTON2 != BIT0) {
-    SET_LED2_ON;
     while (BUTTON2 != BIT0);
-    SET_LED2_OFF;
     return 1;
   } else {
     return 0;
   }
 }
 
-void TIM2_IRQHandler(void)
-{
-  static uint16_t counter=0;
-
-  if ((++counter & 0x0001) == 0)
-    SET_LED2_OFF;
-  else
-    SET_LED2_ON;
-  TIM2->SR &= ~(TIM_SR_UIF);
-}
-
-
-//################## Interrupt Configuration Routine #########################
-void NVIC_Configuration(void)
-{
-  //NVIC_InitTypeDef NVIC_InitStructure;
-
-  ////enable tim2 irq
-  //NVIC_InitStructure.NVIC_IRQChannel = TIM2_IRQn;
-  //NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
-  //NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
-  //NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
-  //NVIC_Init(&NVIC_InitStructure);
-  NVIC_EN0_R |= (1<<TIM2_IRQn); // Enable Timer 2
-}
-
 void main(void)
 {
   board_init();
-  /* Interrupt Config */
   NVIC_Configuration();
   timer_init();
   state = 0;
@@ -123,15 +148,15 @@ void main(void)
   while (1) {
     switch (state) {
       case 0:
-        set_leds(0);
         if (get_button()) {
           state = 1;
+          set_leds(state);
         }
         break;
       case 1:
-        set_leds(1);
         if (get_button()) {
           state = 0;
+          set_leds(state);
         }
         break;
     }
