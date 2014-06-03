6070e043049893a38cfb1374126afc3e621c6842
Berm Mini <mac@mini.berm>
2014-06-04 00:58:19 +0700

    add combo box

--- java/BasicSwing/src/main/java/com/berm/basicswing/App.java
+++ java/BasicSwing/src/main/java/com/berm/basicswing/App.java
@@ -2,16 +2,32 @@ package com.berm.basicswing;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+
 import javax.swing.JButton;
+import javax.swing.JTextField;
+import javax.swing.JTextArea;
+import javax.swing.JLabel;
+import javax.swing.JComboBox;
 
 /**
  * Hello world!
- *
+ * mvn exec:java
  */
 public class App extends JFrame
 {
-    JPanel p = new JPanel();
-    JButton b = new JButton("Hello");
+    JPanel p1 = new JPanel();
+    //JPanel p2 = new JPanel(); // cannot have 2 panels
+    JButton b = new JButton("Hello ");
+    JTextField t = new JTextField("Hi", 20);
+    JTextArea ta = new JTextArea("How\nare\nYou?", 5, 20);
+    JLabel label = new JLabel("What's up");
+    String choices[] = {
+        "Hallow",
+        "Bonjour",
+        "Conichuwa"
+    };
+
+    JComboBox cb = new JComboBox(choices);
 
     public static void main( String[] args )
     {
@@ -24,8 +40,13 @@ public class App extends JFrame
         setSize(400, 300);
         setResizable(true);
 
-        p.add(b);
-        add(p);
+        p1.add(b);
+        p1.add(t);
+        p1.add(ta);
+        p1.add(label);
+        p1.add(cb);
+        add(p1);
+        //add(p2);
 
         setVisible(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
--- java/motor/src/main/java/com/berm/motor/App.java
+++ java/motor/src/main/java/com/berm/motor/App.java
@@ -1,5 +1,7 @@
 package com.berm.motor;
 
+import java.lang.InterruptedException;
+
 /**
  * Hello world!
  *
@@ -11,11 +13,32 @@ public class App
         return honda.toString();
     }
 
-    public static void main( String[] args )
+    public static void main( String[] args ) throws InterruptedException
     {
-        Car ford = new Car("Ford", "Taurus", 2001, 5, 5);
-        System.out.println( "Hello World!" );
-        System.out.println(ford);
-        System.out.println(getHondaCity());
+        MyThread t = new MyThread();
+        Thread y = new Thread(t);
+        y.start();
+        Thread.sleep(2);
+        y.interrupt();
+        System.out.print("Main thread: I've send the interrupt (shutdown) signal\n");
     }
 }
+
+class MyThread implements Runnable {
+    int i=1;
+
+    @Override
+    public void run() {
+        while(Thread.currentThread().isInterrupted() == false) {
+            double a=0.0D;
+            for (int k=100;k>i;--k) {
+                a=k*k*2/5*Math.pow(2, k);
+                a=Math.sqrt(a);
+            }
+            System.out.print("Res (" + i + "): " + a + "\n");
+            i += 1;
+        }
+        System.out.println("I've got an interrupt signal :(\n");
+    }
+}
+
