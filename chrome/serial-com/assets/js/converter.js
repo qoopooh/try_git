50bbf6667d06edf7f6c05e99d5b07503037210a2
Berm Mini <mac@mini.berm>
2014-06-28 22:54:10 +0700

    prepare serial communication

---
+++ chrome/serial-com/assets/js/converter.js
@@ -0,0 +1,99 @@
+(function () {
+  (function () {
+    Converter = function() {
+    };
+
+    /**
+      * input: HEX String Array
+      * output: String
+      */
+    Converter.prototype.hexStringToAscii = function(words, offset, len) {
+      var str = '';
+      var end = words.length;
+
+      if (!offset)
+        offset = 0;
+      if (len > 0)
+        end = offset + len;
+      for (var i = offset; i < end; ++i) {
+        str += String.fromCharCode(parseInt(words[i], 16));
+      }
+
+      return str;
+    };
+
+    /**
+      * input: HEX String Array
+      * output: Uint8Array
+      */
+    Converter.prototype.hexStringToUint8Array = function(str, offset, len) {
+      var end = str.length;
+
+      if (!offset)
+        offset = 0;
+      if (len > 0)
+        end = offset + len;
+      else
+        len = end;
+      var bufView = new Uint8Array(len / 2);
+      j=0;
+      for (var i = offset; i < end; ++i) {
+        hexStr = str[i] + str[++i];
+        bufView[j++] = parseInt(hexStr, 16);
+        //str += String.fromCharCode(parseInt(words[i], 16));
+      }
+      
+      return bufView;
+    };
+
+    /**
+      * input: Uint8
+      * output: HEX String (00)
+      */
+    Converter.prototype.hexString = function(uint8) {
+      var text = uint8.toString(16).toUpperCase();
+      if (text.length === 1)
+        text = '0' + text;
+      return text;
+    }
+
+    /**
+      * input: Uint8Array
+      * output: HEX String
+      */
+    Converter.prototype.abv2hexstr = function(buf) {
+      var msg = "";
+
+      if (buf.length < 1)
+        return msg;
+
+      msg = this.hexString(buf[0]);
+      for (var i=1; i<buf.length; i++) {
+        msg += " " + this.hexString(buf[i]);
+      }
+      return msg;
+    }
+
+    /**
+      * input: ArrayBuffer
+      * output: String
+      */
+    Converter.prototype.ab2str = function(buf) {
+      return String.fromCharCode.apply(null, new Uint8Array(buf));
+    }
+
+    /**
+      * input: String
+      * output: ArrayBuffer
+      */
+    Converter.prototype.str2ab = function(str) {
+      var buf = new ArrayBuffer(str.length);
+      var bufView = new Uint8Array(buf);
+      for (var i=0, strLen=str.length; i<strLen; i++) {
+        bufView[i] = str.charCodeAt(i);
+      }
+      return buf;
+    }
+  }());
+}());
+
---
+++ chrome/serial-com/background.js
@@ -0,0 +1,29 @@
+chrome.app.runtime.onLaunched.addListener(function() {
+  chrome.app.window.create('main.html', winHandler);
+  console.log('launched');
+});
+chrome.runtime.onInstalled.addListener(function() {
+  console.log('installed');
+});
+chrome.runtime.onSuspend.addListener(function() {
+  console.log('suspend');
+});
+chrome.runtime.onMessage.addListener(function(request) {
+  console.log(request);
+  switch (request.type) {
+    case 'report_sockid':
+      console.log('sockId', request.val);
+      break;
+    default:
+      break;
+  }
+});
+
+function winHandler(win) {
+  win.onClosed.addListener(function() {
+    console.log('closed', win.contentWindow.sockId);
+    chrome.socket.disconnect(win.contentWindow.sockId);
+  });
+}
+
+
