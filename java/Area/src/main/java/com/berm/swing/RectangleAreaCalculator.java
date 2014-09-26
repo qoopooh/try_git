4913379d8da7de804d93c16b3b8cba171dd3bd66
Pooh Trusty64 <pooh@x200s.amd64>
2014-09-26 11:37:07 +0700

    Image icon adding does not work

--- java/Area/src/main/java/com/berm/swing/RectangleAreaCalculator.java
+++ java/Area/src/main/java/com/berm/swing/RectangleAreaCalculator.java
@@ -20,7 +20,7 @@ public class RectangleAreaCalculator extends JFrame
     {
         lengthLabel = new JLabel("Please enter the rectangle's length: ");
         widthLabel = new JLabel("Please enter the rectangle's width: ");
-        areaLabel = new JLabel("The Area is: ", SwingConstants.RIGHT);
+        areaLabel = new JLabel("ค่าพื้นที่คือ: ", SwingConstants.RIGHT);
 
         lengthText = new JTextField(12);
         widthText = new JTextField(12);
@@ -29,9 +29,23 @@ public class RectangleAreaCalculator extends JFrame
         calculateButton = new JButton("What's the Area?");
         calculateButtonHandler = new CalculateButtonHandler();
         calculateButton.addActionListener(calculateButtonHandler);
-        exitButton = new JButton("Close");
+        exitButton = new JButton("ปิด");
         exitButtonHandler = new ExitButtonHandler();
         exitButton.addActionListener(exitButtonHandler);
+
+
+        // Add icon does not work
+
+        //Toolkit toolkit = Toolkit.getDefaultToolkit();
+        //MediaTracker tracker = new MediaTracker(this);
+        //Image image = toolkit.getImage("resources/images/d-mcu.png");
+        //tracker.addImage(image, 0);
+        //try {
+            //tracker.waitForAll();
+        //} catch (InterruptedException e) {
+            //e.printStackTrace();
+        //}
+        //setIconImage(image);
 
         setTitle("Rectangle Area Calculator จ้า");
         Container pane = getContentPane();
@@ -76,15 +90,15 @@ public class RectangleAreaCalculator extends JFrame
     private class ExitButtonHandler implements ActionListener
     {
         public void actionPerformed(ActionEvent e) {
-            String name = JOptionPane.showInputDialog(null, "What is your name?");
+            String name = JOptionPane.showInputDialog(null, "คุณชื่ออะไรหรอครับ?");
 
             if (name == null)
                 return;
 
             int choice = JOptionPane.showConfirmDialog(null,
-                    String.format("Do you want really close the app, %s?", name));
+                    String.format("คุณ%s คุณต้องการจะปิดโปรแกรมนี้จริงๆหรอ?", name));
             if (choice == JOptionPane.YES_OPTION) {
-                JOptionPane.showMessageDialog(null, "See you next time, " + name);
+                JOptionPane.showMessageDialog(null, "เจอกันเมื่อชาติต้องการ, " + name);
                 System.exit(0);
             }
         }
