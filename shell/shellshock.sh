71843bd67ff8ac18af98296db5c1b793cb16a833
Pooh Trusty64 <pooh@x200s.amd64>
2014-09-30 21:43:34 +0700

    comment

--- shell/shellshock.sh
+++ shell/shellshock.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
+# If you see the word “vulnerable” in the output, then your system is vulnerable.
+# bash < 4.2
+
 env 'x=() { :;}; echo vulnerable' 'BASH_FUNC_x()=() { :;}; echo vulnerable' bash -c "echo test"
 
