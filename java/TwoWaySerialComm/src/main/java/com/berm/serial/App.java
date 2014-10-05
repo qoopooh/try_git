0e5a51fed9e99d1537163060d8b799eef3e1065c
Pooh Trusty64 <pooh@x200s.amd64>
2014-10-05 09:11:46 +0700

    cannot find SerialPort

--- java/TwoWaySerialComm/src/main/java/com/berm/serial/App.java
+++ java/TwoWaySerialComm/src/main/java/com/berm/serial/App.java
@@ -8,6 +8,11 @@ public class App
 {
     public static void main( String[] args )
     {
+        try {
+            ( new TwoWaySerialComm() ).connect( "/dev/ttyUSB0" );
+        } catch( Exception e ) {
+            e.printStackTrace();
+        }
         System.out.println( "Hello World!" );
     }
 }
