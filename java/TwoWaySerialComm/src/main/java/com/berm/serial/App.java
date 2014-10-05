12a7983d1123c094b6f8589303c11ebd46340ebd
Pooh Trusty64 <pooh@x200s.amd64>
2014-10-05 11:16:27 +0700

    Find SerialPort on sid

--- java/TwoWaySerialComm/src/main/java/com/berm/serial/App.java
+++ java/TwoWaySerialComm/src/main/java/com/berm/serial/App.java
@@ -1,5 +1,9 @@
 package com.berm.serial;
 
+import java.util.List;
+
+import com.berm.serial.util.UartUtil;
+
 /**
  * Hello world!
  *
@@ -8,6 +12,14 @@ public class App
 {
     public static void main( String[] args )
     {
+        List<String> ports = UartUtil.getAvailablePorts();
+        System.out.println( "Found COM port: " + ports.size() );
+        if (ports.size() > 0) {
+            for (String str: ports) {
+                System.out.println( "* " + str );
+            }
+        }
+
         try {
             ( new TwoWaySerialComm() ).connect( "/dev/ttyUSB0" );
         } catch( Exception e ) {
