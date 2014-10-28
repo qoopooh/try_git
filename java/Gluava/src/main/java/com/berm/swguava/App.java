f8d876230b4e92b002e3740102d1830d711a6102
Berm Aae014 <phuchit.aae@gmail.com>
2014-10-28 09:24:54 +0700

    https://code.google.com/p/guava-libraries/wiki/UseGuavaInYourBuild

---
+++ java/Gluava/src/main/java/com/berm/swguava/App.java
@@ -0,0 +1,13 @@
+package com.berm.swguava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
