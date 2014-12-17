b2ad4d75c76c2cdb6fcf66844d415187339c63bc
Pooh Trusty64 <pooh@x200s.amd64>
2014-12-17 22:38:50 +0700

    Add Main-Class on manifest file
    http://www.petrikainulainen.net/programming/gradle/getting-started-with-gradle-our-first-java-project/

---
+++ java/GradleGua/src/main/java/com/berm/guava/Compar.java
@@ -0,0 +1,15 @@
+package com.berm.guava;
+
+public class Compar {
+  public Compar() {
+  }
+
+  public String getName() {
+    return "berm";
+  }
+
+  public static void main(String[] args) {
+    Compar compar = new Compar();
+    System.out.println("Hi: " + compar.getName());
+  }
+}
