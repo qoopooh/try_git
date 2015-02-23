f113e142760ff2e6178b65088fc1e2cdef3eedcd
MrBerm <kanjanakhan@yahoo.com>
2015-02-23 17:20:08 +0700

    Compile pass with gradle 2.3 http://gradle.org/downloads

--- java/RegexValidator/src/main/java/berm/HelloWorld.java
+++ java/RegexValidator/src/main/java/berm/HelloWorld.java
@@ -2,7 +2,7 @@ package berm;
 
 public class HelloWorld {
   public static void main(String[] args) {
-    //RegexValidator validator = new RegexValidator();
+    RegexValidator validator = new RegexValidator(RegexValidator.REGEX_SITE_NAME);
     System.out.println("Hi");
   }
 }
--- java/RegexValidator/src/main/java/berm/RegexValidator.java
+++ java/RegexValidator/src/main/java/berm/RegexValidator.java
@@ -7,6 +7,13 @@ import java.util.regex.Pattern;
 * in the regular expression.
 */
 public class RegexValidator {
+
+  public static final String REGEX_SITE_MASTER_NAME = "^[a-zA-Z0-9]{1}[a-zA-Z0-9\\-\\@\\_\\.]{3,31}$";
+  public static final String REGEX_PASSWORD = "^[a-zA-Z0-9\\!\\@\\#\\$\\%\\&\\*\\-\\_\\+\\=\\{\\}\\[\\]\\|\\~\\;\\:\\,\\.\\/\\?\\<\\>\\\n" +
+      "\\u00C0-\\u017F]{6,32}$";
+  public static final String REGEX_SITE_NAME = "^[a-z0-9]{1}[a-z-0-9\\-]{2,30}[a-z0-9]{1}$";
+  public static final String REGEX_EMAIL = "[A-Za-z0-9!#$%&*+/=?^_`{|}~-\\u00C0-\\u017F]+(?:\\.[A-Za-z0-9!#$%&*+/=?^_`{|}~-\\u00C0-\\u017F]+)*@(?:[a-z0-9\\u00C0-\\u017F](?:[a-z0-9-\\u00C0-\\u017F]*[a-z0-9\\u00C0-\\u017F])?\\.)+[a-z0-9\\u00C0-\\u017F](?:[a-z0-9-\\u00C0-\\u017F]*[a-z0-9\\u00C0-\\u017F])?";
+
   private String mRegex;
 
   /**
