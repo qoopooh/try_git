b0fc8e008e3713bee94baf04752dfbffa9f753e8
Pooh Trusty64 <pooh@x200s.amd64>
2015-04-28 21:38:16 +0700

    - Add rx/tx

--- arduino/extint/extint.ino
+++ arduino/extint/extint.ino
@@ -7,13 +7,22 @@
 #define LED         13
 #define BUTTON      2
 
-const char *k_version = "External Interrupt V0.1";
-const int k_button_delay = 250;
-const int k_count_min = 1400;
+const char *k_version     = "External Interrupt V0.1";
+const int k_button_delay  = 250;
+const int k_buffer_size   = 20;
+const long k_toggle_time  = 1000;
+const long k_interrupt_toggle_time = 200;
 
 volatile int state = LOW;
 unsigned int count;
-unsigned int count_old;
+unsigned int previous_count;
+
+char buffer[k_buffer_size];
+int buffer_index = 0;
+
+long mill         = 0;
+long previous_mill = 0;
+long interval     = 1000;
 
 void setup()
 {
@@ -30,11 +39,43 @@ void setup()
 
 void loop()
 {
-  if (count != count_old) {
-    digitalWrite(LED, !digitalRead(LED));
+  if (count != previous_count) {
+    toggleLED();
     Serial.println(count);
-    count_old = count;
+    previous_count = count;
+    interval = k_interrupt_toggle_time;
   }
+
+  if (Serial.available()) {
+    if (buffer_index < k_buffer_size - 1) {
+      buffer[buffer_index] = Serial.read();
+      if (buffer[buffer_index] == '\r') {
+        buffer[buffer_index + 1] = '\0';
+        buffer_index = 0;
+        printBuffer();
+      } else {
+        ++buffer_index;
+      }
+    } else {
+      buffer[buffer_index] = '\0';
+      buffer_index = 0;
+      printBuffer();
+    }
+  }
+
+  mill = millis();
+
+  if (mill - previous_mill > interval) {
+    previous_mill = mill;
+    toggleLED();
+    interval = k_toggle_time;
+  }
+}
+
+void printBuffer() {
+  Serial.print(buffer);
+  Serial.write(' ');
+  Serial.println(count);
 }
 
 /**
@@ -43,5 +84,9 @@ void loop()
 void blink()
 {
   ++ count;
+}
+
+void toggleLED() {
+  digitalWrite(LED, !digitalRead(LED));
 }
 
