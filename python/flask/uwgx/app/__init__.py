5ca0dab379345bbff0808a83f78a2e1973ebc379
Pi 201505 <Pi@rasp201505>
2015-08-09 09:02:43 +0700

    - Add uwgx config

--- python/flask/uwgx/app/__init__.py
+++ python/flask/uwgx/app/__init__.py
@@ -2,12 +2,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
-__version__ = '0.1'
+__version__ = '0.2'
 
 @app.route('/')
 def index():
     return 'App ' + __version__
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=8000)
 
