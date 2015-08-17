7eb21622a7e65f7c38ec1d9dff0aa9f507cfb887
MrBerm <kanjanakhan@yahoo.com>
2015-08-17 13:18:17 +0700

    - Working session   http://code.runnable.com/UplgZ1HLs9IKAAC4/how-to-use-sessions-in-web-py-for-python-and-webpy

---
+++ python/webpy/session/runnable.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+import web
+
+
+# We have to disable debug mode because it breaks session support
+# There are workarounds for it, however I omit it for the sake of clarity
+web.config.debug = False
+
+# Init our application, this is just about the most basic setup
+#render = web.template.render('/var/www/templates/')
+render = web.template.render('templates')
+urls = (
+    '/', 'Index',
+    '/kill', 'Kill'
+)
+
+app = web.application(urls, globals())
+
+# We create a session handler
+# Here, session data is stored as files in sessions/ folder
+# Session contents at the beginning of a session can be specified with a
+# initializer
+# Session is automatically enabled and cookie is sent to the user
+#session = web.session.Session(app, web.session.DiskStore('/var/www/sessions'),
+session = web.session.Session(app, web.session.DiskStore('sessions'),
+    initializer={'count': 0})
+
+class Index:
+    def GET(self):
+        # Get the data from session, increment it and display the page
+        # Saving the data is done automatically
+        refreshes = session.count
+        session.count += 1
+        return render.sessions(count=refreshes)
+
+class Kill:
+    def GET(self):
+        # Remove session data
+        # And redirect the user to the main page
+        session.kill()
+        return web.seeother('/')
+
+application = app.wsgifunc()
+
+if __name__=="__main__":
+    app.run()
