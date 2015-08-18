a71629de7d2fcc50e897602226e21b40b3574e4e
Pi 201505 <Pi@rasp201505>
2015-08-18 14:53:11 +0700

    - Make flask wsgi debugable

--- python/flask/uwgx/app/__init__.py
+++ python/flask/uwgx/app/__init__.py
@@ -1,13 +1,15 @@
+#!/usr/bin/env python
+
 from flask import Flask
 
 app = Flask(__name__)
 
-__version__ = '0.2'
+__version__ = '0.1'
 
 @app.route('/')
 def index():
     return 'App ' + __version__
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host='0.0.0.0', port=8000, debug=True)
 
