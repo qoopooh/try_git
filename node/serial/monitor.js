18730c0d5150dae0ce8a4563f6ccc2dcdb5f9899
Berm Mac <berm@mac.240>
2016-05-27 22:38:23 +0700

    monitor.js - preset BT module

--- node/serial/monitor.js
+++ node/serial/monitor.js
@@ -1,31 +1,38 @@
 #!/usr/bin/env nodejs
 
+var usb = '/dev/ttyUSB0'; // Linux style
+/*var usb = '/dev/tty.wchusbserial24120'; // mac style */
+
+process.argv.forEach(function (val, index, array) {
+   if (index > 1)
+      usb = val;
+});
+
 var SerialPort = require('serialport').SerialPort;
-var port = new SerialPort('/dev/ttyUSB0', {
+var port = new SerialPort(usb, {
   baudrate: 9600,
 });
 
-/*port.on('open', function () {*/
-/*port.write('main screen turn on', function(err, bytesWritten) {*/
-/*if (err) {*/
-/*return console.log('Error: ', err.message);*/
-/*}*/
-/*console.log(bytesWritten, 'bytes written');*/
-/*});*/
-/*});*/
+port.on('open', function () {
+  port.write('AT+NOTI1\r\n', function(err, bytesWritten) {
+    // NOTI1 to show -> OK+LOST\r\n and OK+CONN\r\n
+    if (err) {
+      return console.log('Error: ', err.message);
+    }
+    console.log(bytesWritten, 'bytes written');
+  });
+});
 
 port.on('data', function (data) {
   console.log('Data length: ' + data.length);
+  data = ("" + data).replace('\r', '\\r');
+  data = data.replace('\n', '\\n\n\n');
   if (data.length == 1) {
-    if (data == '\r')
-      data = 'CR';
-    else if (data == '\n')
-      data = 'NL\n\n';
     console.log('Data 1: ' + data);
   } else {
     console.log('Multi Data: ' + data);
   }
 });
 
-console.log('Hi');
+console.log('Open ' + usb + '\n');
 
