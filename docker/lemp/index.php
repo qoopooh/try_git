0cae6d229bf747c69cf36a6141abead79ad48d06
Berm Quad <berm@simple-different.com>
2016-06-07 08:29:15 +0700

    Docker compose

--- docker/lemp/index.php
+++ docker/lemp/index.php
@@ -5,6 +5,31 @@
     <title>Hello World!</title>
   </head>
   <body>
-    <img src="http://blog.osteel.me/images/2015/12/18/docker-tutorial2.gif" alt="Hello World!" />
+    <?php
+      $database   = "sd_app";
+      $user       = "root";
+      $password   = "sddba";
+      $host       = "mysql";
+      $connection = new PDO("mysql:host={$host};dbname={$database};charset=utf8", $user, $password);
+      //$query      = $connection->query("SELECT TABLE_NAME FROM information_schema.TABLES WHERE TABLE_TYPE='BASE TABLE'");
+      //$result      = $connection->query("SELECT * FROM w_user");
+      $result      = $connection->query("SELECT * FROM w_user");
+
+      if ($result)
+        echo "<p>1</p>";
+      else
+        echo "<p>2</p>";
+      //if ($result->num_rows < 1) {
+        //echo "<p>There are no row in query \"{$database}\".</p>";
+      //} else {
+        //echo "<p>Database \"{$database}\" has the following row:</p>";
+        //echo "<ul>";
+
+        //while($row = $result->fetch_assoc()) {
+          //echo "<li>id: " . $row["id"]. " - Name: </li>";
+        //}
+        //echo "</ul>";
+      //}
+    ?>
   </body>
 </html>
