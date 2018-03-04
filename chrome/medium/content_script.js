b71ed8fcbce68f357b287900037e3dd17d3a7714
Berm MBA11 <berm@mba11.mac>
2018-03-04 23:55:50 +0700

    medium: manage header/footer tags and ids

--- chrome/medium/content_script.js
+++ chrome/medium/content_script.js
@@ -1,20 +1,40 @@
 (function() {
 
   // remove medium bars
-  var f = document.getElementsByClassName("u-fixed");
-  for (i in f) {
-    if (f[i].style) {
-      f[i].style.display = "none";
+  var mediumbars = document.getElementsByClassName("u-fixed");
+  for (i in mediumbars) {
+    if (mediumbars[i].style) {
+      mediumbars[i].style.display = "none";
     }
   }
 
   // remove header tag
-  var h = document.getElementsByTagName('header');
-  for (i in h) {
-    if (h[i].style) {
-      h[i].style.display = "none";
+  var headers = document.getElementsByTagName('header');
+  for (i in headers) {
+    if (headers[i].style) {
+      headers[i].style.display = "none";
     }
   }
 
-  console.log('hidden');
+  // remove footer tag
+  var footers = document.getElementsByTagName('footer');
+  for (i in footers) {
+    if (footers[i].style) {
+      footers[i].style.display = "none";
+    }
+  }
+
+  // remove header id
+  var header = document.getElementById('header');
+  if (header && header.style) {
+    header.style.display = "none";
+  }
+
+  // remove footer id
+  var footer = document.getElementById('footer');
+  if (footer && footer.style) {
+    footer.style.display = "none";
+  }
+
+  console.log('medium is hidden');
 })();
