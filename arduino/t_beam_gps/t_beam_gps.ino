b996667349d9652e59e1a269233a53ae484bb0d5
berm MBA 2017 <berm@Air13.local>
2021-06-05 11:18:21 +0700

    RoLa sent / received

--- arduino/t_beam_gps/t_beam_gps.ino
+++ arduino/t_beam_gps/t_beam_gps.ino
@@ -1,131 +1,78 @@
-#include "src/gps/gps.h"
-//#include "src/oled/oled.h"
-//#include "src/lorap2p/lorap2p.h"
+#include <LoRa.h>
+#include "boards.h"
 
-#define GPS_ACCURACY 7
-#define PERIOD 2000000
-const int LED = 4;
+int counter = 0;
 
-byte localAddress = 0xAA;
-byte peerAddress = 0xBB;
+void getLocation(char * buf);
 
-unsigned long localGPSFixMillis;
-unsigned long receivedGPSFixMillis;
-unsigned long lastKnownHaversineDistanceMillis;
+void setup()
+{
+    initBoard();
+    // When the power is turned on, a delay is required.
+    delay(1500);
 
-long lastSendTime = 0;
-int interval = PERIOD;
-long haversine_distance = 0.0;
-long count = 0;
-
-LatLong localLatlong = {0.0, 0.0};
-LatLong lastKnownLocalLatlong = {0.0, 0.0};
-LatLong destinationLatlong = {0.0, 0.0};
-
-void setup() {
-  Serial.begin(9600);
-  Serial.println("Start LoRa GPS duplex");
-
-  pinMode(LED, OUTPUT);     
-  initGPS();
-  //initOLED();
-  //initLora();
-
-  //displayInitOLED();
-
-  Serial.println("initiated");
-}
-
-void loop() {
-  while (isGPSAvailable()) {
-    getLatLong(&localLatlong);
-  }
-
-  //if (receiveLora(localAddress, &destinationLatlong.latitude, &destinationLatlong.longitude)) {
-    //receivedGPSFixMillis = millis();
-    //printStatus("Received", &destinationLatlong, peerAddress, localAddress);
-  //}
-
-  if (millis() - lastSendTime > interval) {
-    delay(2000);
-    toggleLED();
-    //Serial.println("loop");
-    if (isGPSValid(&localLatlong)) {
-
-      // If new GPS lat-long is received
-      // Then update timestamp
-      // Then send to peer LoRa node
-      if (!isGPSsameAsLastKnown(&lastKnownLocalLatlong, &localLatlong)) {
-        localGPSFixMillis = millis();
-
-        String latlongData = String(localLatlong.latitude, GPS_ACCURACY) + "," + String(localLatlong.longitude, GPS_ACCURACY);
-        //sendLora(latlongData, localAddress, peerAddress);
-
-        lastKnownLocalLatlong.latitude = localLatlong.latitude;
-        lastKnownLocalLatlong.longitude = localLatlong.longitude;
-      }
-
-      // Always display on the OLED with relative time ago
-      String localMillisStr;
-      getReadableTime(localGPSFixMillis, localMillisStr);
-
-      Serial.print("Lat-Long: ");
-      Serial.print(localLatlong.latitude, 7);
-      Serial.print(",");
-      Serial.print(localLatlong.longitude, 7);
-      Serial.print(" at ");
-      Serial.print(localGPSFixMillis);
-      Serial.print("ms. ");
-      Serial.print(localMillisStr);
-      Serial.println(" ago");
+    Serial.println("LoRa Sender");
+    LoRa.setPins(RADIO_CS_PIN, RADIO_RST_PIN, RADIO_DI0_PIN);
+    if (!LoRa.begin(LoRa_frequency)) {
+        Serial.println("Starting LoRa failed!");
+        while (1);
     }
-  }
 }
 
-void printStatus(String status, struct LatLong *ll, byte addressA, byte addressB) {
-  Serial.print(status + " latlong ");
-  Serial.print(ll->latitude, GPS_ACCURACY);
-  Serial.print(",");
-  Serial.print(ll->longitude, GPS_ACCURACY);
-  Serial.print(" from 0x" + String(addressA, HEX));
-  Serial.println(" to 0x" + String(addressB, HEX));
+void loop()
+{
+    Serial.print("Sending packet:");
+    Serial.println(counter);
+
+    // send packet
+    LoRa.beginPacket();
+    LoRa.print("hello ");
+    LoRa.print(counter);
+    LoRa.endPacket();
+
+#ifdef HAS_DISPLAY
+    if (u8g2) {
+        char buf[256];
+        u8g2->clearBuffer();
+        u8g2->drawStr(0, 12, "Transmitting: OK!");
+        snprintf(buf, sizeof(buf), "Sending: %d", counter);
+        u8g2->drawStr(0, 30, buf);
+        u8g2->sendBuffer();
+    }
+#endif
+#ifdef HAS_OLED
+    char buf[256];
+    display.clear();
+    display.setTextAlignment(TEXT_ALIGN_LEFT);
+
+    display.setFont(ArialMT_Plain_10);
+    display.drawString(0, 12, "Transmitting: OK!");
+
+    snprintf(buf, sizeof(buf), "Sending: %d", counter);
+    display.setFont(ArialMT_Plain_16);
+    display.drawString(0, 24, buf);
+
+#ifdef HAS_GPS
+    display.setFont(ArialMT_Plain_10);
+    getLocation(buf);
+    display.drawString(0, 48, buf);
+#endif
+
+    display.display();
+#endif
+
+    counter++;
+    Serial.println("Sleep for 2 seconds");
+    delay(2000);
 }
 
-void getReadableTime(long millisTime, String &readableTime) {
-  unsigned long seconds = (millis() - millisTime) / 1000;
-  unsigned long minutes = seconds / 60;
-  unsigned long hours = minutes / 60;
-  unsigned long days = hours / 24;
-  millisTime %= 1000;
-  seconds %= 60;
-  minutes %= 60;
-  hours %= 24;
-
-  if (days > 0) {
-    readableTime = String(days) + " ";
+void getLocation(char * buf) {
+#ifdef HAS_GPS
+  if (Serial1.available() > 0) {
+      if (gps.encode(Serial1.read())) {
+          Serial.println("Got GPS");
+      }
   }
-
-  if (hours > 0) {
-    readableTime += String(hours) + ":";
-  }
-
-  if (minutes < 10) {
-    readableTime += "0";
-  }
-  readableTime += String(minutes) + ":";
-
-  if (seconds < 10) {
-    readableTime += "0";
-  }
-  readableTime += String(seconds);
-}
-
-void toggleLED() {
-  ++count;
-
-  if (count % 2 == 0) {
-    digitalWrite(LED, HIGH);
-  } else {
-    digitalWrite(LED, LOW);
-  }
+  snprintf(buf, 32, "%f, %f", gps.location.lat(), gps.location.lng());
+#endif
 }
